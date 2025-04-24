ab2a30de96de02f84b3ae0f6c012f96e05122d95
ahehead <nojoycrystal@gmail.com>
2025-04-24 21:17:41 +0900

    Nodeエディタ用の基本ノードと実行ボタンコンポーネントを追加

---
+++ src/renderer/nodeEditor/nodes/BasicNodes.tsx
@@ -0,0 +1,64 @@
+import type { JSX } from "react";
+import { ClassicPreset } from "rete";
+import RunButton from "./RunButton";
+
+export class Node extends ClassicPreset.Node<
+  { [key in string]: ClassicPreset.Socket },
+  { [key in string]: ClassicPreset.Socket },
+  {
+    [key in string]:
+    | PromptControl
+    | ClassicPreset.Control
+    | ClassicPreset.InputControl<"number">
+    | ClassicPreset.InputControl<"text">;
+  }
+> { }
+
+// テキスト専用ソケット
+export class StringSocket extends ClassicPreset.Socket {
+  constructor() {
+    super("string");
+  }
+  isCompatibleWith(socket: ClassicPreset.Socket): boolean {
+    return socket instanceof StringSocket;
+  }
+}
+
+// String入力ノード用コントロール（テキスト入力）
+export class StringNode extends ClassicPreset.Node<
+  object,
+  { value: ClassicPreset.Socket },
+  { textInput: ClassicPreset.InputControl<"text"> }
+> {
+  constructor() {
+    super("String");
+    this.addOutput("value", new ClassicPreset.Output(new StringSocket()));
+    this.addControl(
+      "textInput",
+      new ClassicPreset.InputControl("text", { initial: "" })
+    );
+  }
+}
+
+// 長文プロンプト入力ノード用コントロール（テキストエリア）
+export class PromptControl extends ClassicPreset.InputControl<"text"> {
+  public value: string;
+  constructor(initial = "") {
+    super("text");
+    this.value = initial;
+  }
+}
+
+// 実行buttonコントロール
+export class RunButtonControl extends ClassicPreset.Control {
+  constructor(
+    public label: string,
+    public onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+  ) {
+    super()
+  }
+}
+
+export function CustomButton(props: { data: RunButtonControl }): JSX.Element {
+  return <RunButton label={props.data.label} onClick={props.data.onClick} />
+}
