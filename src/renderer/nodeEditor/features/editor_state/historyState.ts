25919ac4a88a9617ab990cd7cc6b4856176557f8
ahehead <nojoycrystal@gmail.com>
2025-04-30 00:49:13 +0900

    履歴管理機能を整理し、historyState.tsを新規作成して初期化処理を実装

---
+++ src/renderer/nodeEditor/features/editor_state/historyState.ts
@@ -0,0 +1,78 @@
+import type { NodeEditor } from "rete";
+import type { AreaPlugin } from "rete-area-plugin";
+import type { DataflowEngine, ControlFlowEngine } from "rete-engine";
+import type { HistoryActions, HistoryPlugin } from "rete-history-plugin";
+import { exportGraph } from "shared/exportGraph";
+import { createNodes } from "../createGraph/createNodes";
+import { AreaExtensions } from "rete-area-plugin";
+import type { AreaExtra, Schemes } from "../../types";
+import type { GraphJsonData } from "shared/JsonType";
+
+export interface NodeEditorState {
+  graph: GraphJsonData;
+  historyState: HistoryState;
+}
+
+export function createNodeEditorState(graph: GraphJsonData): NodeEditorState {
+  return {
+    graph: graph,
+    historyState: initializeHistoryState(),
+  };
+}
+
+export interface HistoryState {
+  active: boolean;
+  produced: any[];
+  reserved: any[];
+  limit?: number;
+}
+
+export function getCurrentEditorState(
+  editor: NodeEditor<Schemes>,
+  area: AreaPlugin<Schemes, AreaExtra>,
+  history: HistoryPlugin<Schemes, HistoryActions<Schemes>>
+): NodeEditorState {
+  return {
+    graph: exportGraph(editor, area),
+    historyState: {
+      active: (history as any).history.active,
+      produced: (history as any).history.produced.slice(),
+      reserved: (history as any).history.reserved.slice(),
+      limit: (history as any).history.limit,
+    },
+  };
+}
+
+export function initializeHistoryState(): HistoryState {
+  return {
+    active: false,
+    produced: [],
+    reserved: [],
+    limit: undefined,
+  };
+}
+
+export async function resetEditorState(
+  payload: NodeEditorState,
+  editor: NodeEditor<Schemes>,
+  area: AreaPlugin<Schemes, AreaExtra>,
+  dataflow: DataflowEngine<Schemes>,
+  engine: ControlFlowEngine<Schemes>,
+  history: HistoryPlugin<Schemes, HistoryActions<Schemes>>
+): Promise<void> {
+  await editor.clear();
+  history.clear();
+  await createNodes(payload.graph, area, editor, dataflow, engine, history);
+  setHistoryState(history, payload);
+  await AreaExtensions.zoomAt(area, editor.getNodes());
+}
+
+function setHistoryState(
+  history: HistoryPlugin<Schemes, HistoryActions<Schemes>>,
+  payload: NodeEditorState
+) {
+  (history as any).history.active = payload.historyState.active;
+  (history as any).history.produced = payload.historyState.produced.slice();
+  (history as any).history.reserved = payload.historyState.reserved.slice();
+  (history as any).history.limit = payload.historyState.limit;
+}
