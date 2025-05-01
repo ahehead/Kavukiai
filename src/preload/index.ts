26baa72d7c0ff92f868fbf693fcbae748f22cae9
ahehead <nojoycrystal@gmail.com>
2025-05-01 22:19:38 +0900

    APIのメソッド名を変更し、状態管理の明確化を図る

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -13,23 +13,52 @@ declare global {
 }
 
 export type AppApi = {
-  loadAppState(): Promise<MainState>;
+  loadAppStateSnapshot(): Promise<MainState>;
   saveApiKey(key: string | null): Promise<ApiKeysFlags>;
   openAIRequest(params: OpenAIParams): Promise<string>;
   onOpenSettings(callback: () => void): void;
-  saveAppState(state: PersistedMainState): void;
+  takeAppStateSnapshot(state: PersistedMainState): void;
+  onSaveGraphInitiate(callback: () => void): () => Electron.IpcRenderer;
+  showSaveDialog(title: string): Promise<string | null>;
+  saveGraphJsonData(filePath: string, graph: unknown): Promise<boolean>;
 };
 
 const API: AppApi = {
-  loadAppState: () => ipcRenderer.invoke(IpcChannel.LoadState),
+  // アプリの状態を復元
+  loadAppStateSnapshot: () => ipcRenderer.invoke(IpcChannel.LoadState),
+
   saveApiKey: (key) =>
     ipcRenderer.invoke(IpcChannel.SaveApiKey, key) as Promise<ApiKeysFlags>,
   openAIRequest: (params: OpenAIParams) =>
     ipcRenderer.invoke(IpcChannel.OpenAIRequest, params),
   onOpenSettings: (callback) =>
     ipcRenderer.on(IpcChannel.OpenSettings, () => callback()),
-  saveAppState: (state: PersistedMainState) =>
+
+  // アプリの状態をスナップショットする
+  takeAppStateSnapshot: (state: PersistedMainState) =>
     ipcRenderer.send(IpcChannel.SaveState, state),
+
+  // mainからのファイル保存要請
+  onSaveGraphInitiate: (callback): (() => Electron.IpcRenderer) => {
+    const listener = () => callback();
+    ipcRenderer.on(IpcChannel.SaveGraphInitiate, listener);
+    // 解除用関数を返す
+    return () =>
+      ipcRenderer.removeListener(IpcChannel.SaveGraphInitiate, listener);
+  },
+  // ダイアログを開く
+  showSaveDialog: (title: string) =>
+    ipcRenderer.invoke(IpcChannel.ShowSaveDialog, title) as Promise<
+      string | null
+    >,
+
+  // グラフを保存
+  saveGraphJsonData: (filePath, graph) =>
+    ipcRenderer.invoke(
+      IpcChannel.SaveGraph,
+      filePath,
+      graph
+    ) as Promise<boolean>,
 };
 
 contextBridge.exposeInMainWorld("App", API);
