31fe7b6c90c26ed1219e94bdd21d9db9d394b73d
ahehead <nojoycrystal@gmail.com>
2025-05-02 17:44:59 +0900

    ApplicationSettingsの構造を整理し、windowSettingsとsystemSettingsを明示的に分けて定義

--- src/main/types.ts
+++ src/main/types.ts
@@ -1,23 +1,34 @@
 export type ApplicationSettings = {
+  windowSettings: WindowSettings;
+  systemSettings: SystemSettings;
+};
+
+export type WindowSettings = {
   width: number;
   height: number;
   x: number;
   y: number;
   isMaximized: boolean;
   isFullscreen: boolean;
+};
 
+export type SystemSettings = {
   // 前回の保存フォルダ
   lastSaveDir: string | null;
 };
 
 export function createDefaultApplicationSettings(): ApplicationSettings {
   return {
-    width: 1200,
-    height: 800,
-    x: 0,
-    y: 0,
-    isMaximized: false,
-    isFullscreen: false,
-    lastSaveDir: null,
+    windowSettings: {
+      width: 1200,
+      height: 800,
+      x: 0,
+      y: 0,
+      isMaximized: false,
+      isFullscreen: false,
+    },
+    systemSettings: {
+      lastSaveDir: null,
+    },
   };
 }
