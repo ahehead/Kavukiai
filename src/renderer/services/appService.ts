4753c6c126b55c5cb7b58c37eddf63fca1c11581
ahehead <nojoycrystal@gmail.com>
2025-05-03 13:35:28 +0900

    electronApiServiceの型定義を整理し、メソッドの列挙を追加

--- src/renderer/services/appService.ts
+++ src/renderer/services/appService.ts
@@ -1,39 +1,16 @@
-import type { MainState, PersistedMainState } from "shared/AppType";
-import type { CloseFileDialogResponse } from "shared/ApiType";
-import type { GraphJsonData } from "shared/JsonType";
-
 const { App } = window;
 
-export const electronApiService = {
-  showCloseConfirm: (): Promise<{ response: CloseFileDialogResponse }> =>
-    App.showCloseConfirm(),
+// 対象メソッド名を列挙
+type ApiKeys =
+  | "showCloseConfirm"
+  | "showSaveDialog"
+  | "saveGraphJsonData"
+  | "loadAppStateSnapshot"
+  | "takeAppStateSnapshot"
+  | "onOpenSettings"
+  | "onSaveGraphInitiate"
+  | "onFileLoadedRequest";
 
-  showSaveDialog: (title: string): Promise<string | null> =>
-    App.showSaveDialog(title),
+type ElectronService = Pick<typeof App, ApiKeys>;
 
-  saveGraphJsonData: (
-    filePath: string,
-    graph: GraphJsonData,
-    lastHash: string
-  ): Promise<string | null> => App.saveGraphJsonData(filePath, graph, lastHash),
-
-  loadAppStateSnapshot: (): Promise<MainState> => App.loadAppStateSnapshot(),
-
-  takeAppStateSnapshot: (state: PersistedMainState): void =>
-    App.takeAppStateSnapshot(state),
-
-  onOpenSettings: (handler: () => void): (() => void) =>
-    App.onOpenSettings(handler),
-
-  onSaveGraphInitiate: (handler: () => Promise<boolean>): (() => void) =>
-    App.onSaveGraphInitiate(handler),
-
-  onFileLoadedRequest: (
-    handler: (
-      e: Electron.IpcRendererEvent,
-      path: string,
-      name: string,
-      json: GraphJsonData
-    ) => Promise<void>
-  ): (() => void) => App.onFileLoadedRequest(handler),
-};
+export const electronApiService = App satisfies ElectronService;
