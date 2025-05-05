c61472b34a49d9a367ab3ce8fca1987070441a64
ahehead <nojoycrystal@gmail.com>
2025-05-06 05:01:38 +0900

    UIButtonコンポーネントを更新し、BellButtonを追加、iconButtonVariantsの定義を整理

--- src/renderer/components/atom/UIButton.tsx
+++ src/renderer/components/atom/UIButton.tsx
@@ -1,14 +1,31 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "renderer/lib/utils";
 import { Button, buttonVariants } from "../ui/button";
-import { Circle, Plus, X } from "lucide-react";
+import { Bell, BellPlus, Circle, Plus, X } from "lucide-react";
 
-export const iconButtonVariants = cva("flex-shrink-0 w-6 h-6 flex items-center justify-center rounded-md cursor-pointer bg-transparent focus:outline-none forcus-visible:ounline-none shadow-none")
+export const iconButtonVariants = cva("flex-shrink-0 rounded-md cursor-pointer bg-transparent focus:outline-none forcus-visible:outline-none", {
+  variants: {
+    variant: {
+      default: "shadow-none",
+      outline: "rounded-full bg-background",
+    },
+    outLineSize: {
+      md: "w-6 h-6",
+      lg: "w-8 h-8",
+    }
+  },
+  defaultVariants: {
+    variant: "default",
+    outLineSize: "md",
+  }
+})
+
+
 
 
 export function UIButton({ ...props }: React.ComponentProps<"button"> & VariantProps<typeof iconButtonVariants>) {
   return (
-    <Button className={cn(buttonVariants({ variant: "ghost" }), iconButtonVariants())} {...props} />
+    <Button className={cn(buttonVariants({ variant: "ghost", size: "icon" }), iconButtonVariants())} {...props} />
   )
 }
 
@@ -34,5 +51,15 @@ export function CloseFileButton({ isDirty, ...props }: { isDirty: boolean } & Re
         <X className="icon-base-size" />
       )}
     </UIButton>
+  )
+}
+
+export function BellButton({ haveUnread, ...props }: { haveUnread: boolean } & React.ComponentProps<"button">) {
+  return (
+    <Button className={cn(buttonVariants({ variant: "outline" }), iconButtonVariants({ outLineSize: "lg", variant: "outline" }))} {...props}>
+      {haveUnread ?
+        <BellPlus className="icon-size-md" />
+        : <Bell className="icon-size-md" />}
+    </Button>
   )
 }
