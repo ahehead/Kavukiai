a9ed154772a01db494d49ed406472dc2d55d6f8f
ahehead <nojoycrystal@gmail.com>
2025-05-08 22:56:12 +0900

    feat: ClassicPreset.SocketからCustomSocketTypeへの移行

--- src/renderer/nodeEditor/nodes/Sockets.ts
+++ src/renderer/nodeEditor/nodes/Sockets.ts
@@ -1,51 +1,56 @@
-import { ClassicPreset } from "rete";
+import { CustomSocketType } from "../types";
 
 // テキスト専用ソケット
-export class StringSocket extends ClassicPreset.Socket {
+export class StringSocket extends CustomSocketType {
   constructor() {
     super("string");
+    this.type = "string";
   }
-  isCompatibleWith(socket: ClassicPreset.Socket): boolean {
+  isCompatibleWith(socket: CustomSocketType): boolean {
     return socket instanceof StringSocket;
   }
 }
 
 // 実行トリガー用ソケット
-export class ExecSocket extends ClassicPreset.Socket {
+export class ExecSocket extends CustomSocketType {
   constructor() {
     super("exec");
+    this.type = "exec";
   }
-  isCompatibleWith(socket: ClassicPreset.Socket): boolean {
+  isCompatibleWith(socket: CustomSocketType): boolean {
     return socket instanceof ExecSocket;
   }
 }
 
 // チャットコンテキスト用ソケット
-export class ContextSocket extends ClassicPreset.Socket {
+export class ContextSocket extends CustomSocketType {
   constructor() {
     super("context");
+    this.type = "context";
   }
-  isCompatibleWith(socket: ClassicPreset.Socket): boolean {
+  isCompatibleWith(socket: CustomSocketType): boolean {
     return socket instanceof ContextSocket;
   }
 }
 
 // 設定用ソケット
-export class SettingSocket extends ClassicPreset.Socket {
+export class SettingSocket extends CustomSocketType {
   constructor() {
     super("setting");
+    this.type = "setting";
   }
-  isCompatibleWith(socket: ClassicPreset.Socket): boolean {
+  isCompatibleWith(socket: CustomSocketType): boolean {
     return socket instanceof SettingSocket;
   }
 }
 
 // 画像用ソケット
-export class ImageSocket extends ClassicPreset.Socket {
+export class ImageSocket extends CustomSocketType {
   constructor() {
     super("image");
+    this.type = "image";
   }
-  isCompatibleWith(socket: ClassicPreset.Socket): boolean {
+  isCompatibleWith(socket: CustomSocketType): boolean {
     return socket instanceof ImageSocket;
   }
 }
