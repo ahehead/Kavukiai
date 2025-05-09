734b0be18705a383c7b4c601164ed68997bbb716
ahehead <nojoycrystal@gmail.com>
2025-05-10 02:58:19 +0900

    fix: MultiLineStringNodeのプロパティをオプショナルに変更し、インポートを整理

--- src/renderer/nodeEditor/nodes/Node/MultiLineStringNode.tsx
+++ src/renderer/nodeEditor/nodes/Node/MultiLineStringNode.tsx
@@ -2,7 +2,7 @@ import { ClassicPreset } from 'rete';
 import { StringSocket } from '../Sockets';
 import { MultiLineControl } from '../Controls/TextArea';
 import type { HistoryPlugin } from 'rete-history-plugin';
-import type { AreaExtra, Schemes } from 'renderer/nodeEditor/types';
+import type { AreaExtra, ExtraSizeData, Schemes } from 'renderer/nodeEditor/types';
 import type { AreaPlugin } from 'rete-area-plugin';
 
 // 長文文字列入力ノード
@@ -10,12 +10,15 @@ export class MultiLineStringNode extends ClassicPreset.Node<
   object,
   { out: ClassicPreset.Socket },
   { textArea: MultiLineControl }
-> {
-  public width = 180;
-  public height = 199;
-  constructor(initial = '',
+> implements ExtraSizeData {
+  public width?: number;
+  public height?: number;
+
+  constructor(
+    initial = '',
     history?: HistoryPlugin<Schemes>,
-    area?: AreaPlugin<Schemes, AreaExtra>) {
+    area?: AreaPlugin<Schemes, AreaExtra>
+  ) {
     super('MultiLineString');
     this.addOutput('out', new ClassicPreset.Output(new StringSocket(), 'string'));
     this.addControl('textArea', new MultiLineControl(initial, undefined, true, history, area));
