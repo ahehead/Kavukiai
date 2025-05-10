2a136f91014e7060f5baf4c23cdead4391b01ef8
ahehead <nojoycrystal@gmail.com>
2025-05-11 07:02:41 +0900

    fix: CustomSocketコンポーネントの実装を簡素化し、NodeDataSocketを使用

--- src/renderer/nodeEditor/custom/CustomSocket.tsx
+++ src/renderer/nodeEditor/custom/CustomSocket.tsx
@@ -1,3 +1,4 @@
+import { NodeDataSocket } from "renderer/components/NodePanel"
 import type { ClassicPreset } from "rete"
 
 export function CustomSocket<T extends ClassicPreset.Socket>({
@@ -6,16 +7,6 @@ export function CustomSocket<T extends ClassicPreset.Socket>({
   data: T
 }): React.ReactElement {
   return (
-    <div className="group cursor-pointer flex socket-icon-wrapper-size rounded-full items-center justify-center">
-      <div
-        className="
-              socket-icon-size
-               rounded-full bg-dataSocket
-               border-[#686dcc] border-[1px]
-               transform transition-all duration-100
-               group-hover:scale-115"
-        title={data.name}
-      />
-    </div>
+    <NodeDataSocket title={data.name} />
   )
 }
