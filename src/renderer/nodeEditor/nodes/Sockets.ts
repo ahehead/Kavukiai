60c5ab00e1dcb1642c391059973a0d562cd3f220
ahehead <nojoycrystal@gmail.com>
2025-05-11 04:02:34 +0900

    refactor: 不要なソケットクラスを削除し、createSocket関数を追加

--- src/renderer/nodeEditor/nodes/Sockets.ts
+++ src/renderer/nodeEditor/nodes/Sockets.ts
@@ -1,56 +1,7 @@
 import { CustomSocketType } from "../types";
 
-// テキスト専用ソケット
-export class StringSocket extends CustomSocketType {
-  constructor() {
-    super("string");
-    this.type = "string";
-  }
-  isCompatibleWith(socket: CustomSocketType): boolean {
-    return socket instanceof StringSocket;
-  }
-}
+type SocketType = "string" | "exec" | "context" | "setting" | "image";
 
-// 実行トリガー用ソケット
-export class ExecSocket extends CustomSocketType {
-  constructor() {
-    super("exec");
-    this.type = "";
-  }
-  isCompatibleWith(socket: CustomSocketType): boolean {
-    return socket instanceof ExecSocket;
-  }
-}
-
-// チャットコンテキスト用ソケット
-export class ContextSocket extends CustomSocketType {
-  constructor() {
-    super("context");
-    this.type = "context";
-  }
-  isCompatibleWith(socket: CustomSocketType): boolean {
-    return socket instanceof ContextSocket;
-  }
-}
-
-// 設定用ソケット
-export class SettingSocket extends CustomSocketType {
-  constructor() {
-    super("setting");
-    this.type = "setting";
-  }
-  isCompatibleWith(socket: CustomSocketType): boolean {
-    return socket instanceof SettingSocket;
-  }
-}
-
-// 画像用ソケット
-export class ImageSocket extends CustomSocketType {
-  constructor() {
-    super("image");
-    this.type = "image";
-  }
-  isCompatibleWith(socket: CustomSocketType): boolean {
-    return socket instanceof ImageSocket;
-  }
+export function createSocket(socketType: SocketType): CustomSocketType {
+  return new CustomSocketType(socketType);
 }
