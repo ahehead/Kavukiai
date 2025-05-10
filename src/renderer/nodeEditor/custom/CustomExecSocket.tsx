d42be81837901541f6361d1f080fa04274ee9b93
ahehead <nojoycrystal@gmail.com>
2025-05-11 07:02:47 +0900

    fix: CustomExecSocketコンポーネントの実装を簡素化し、TriangleIconの使用を削除

--- src/renderer/nodeEditor/custom/CustomExecSocket.tsx
+++ src/renderer/nodeEditor/custom/CustomExecSocket.tsx
@@ -1,5 +1,5 @@
+import { NodeExecSocket } from 'renderer/components/NodePanel'
 import type { ClassicPreset } from 'rete'
-import TriangleIcon from 'src/resources/public/triangleIcon/triangle.svg?react'
 
 export type ExtraSocketData = {
   isConnectable?: boolean
@@ -11,21 +11,6 @@ export function CustomExecSocket<T extends ClassicPreset.Socket>(props: {
   const { data } = props
 
   return (
-    <div className="group cursor-pointer flex socket-icon-wrapper-size rounded-full items-center justify-center">
-      <div
-        className="
-        rounded-full
-        transform transition-all duration-100
-        group-hover:scale-115
-        "
-        title={data.name}
-      >
-        <TriangleIcon
-          className="socket-icon-size
-                     fill-[var(--color-execSocket)]
-                     stroke-[#f2ffee]"
-        />
-      </div>
-    </div>
+    <NodeExecSocket title={data.name} />
   )
 }
