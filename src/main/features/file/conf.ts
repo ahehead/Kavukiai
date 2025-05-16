02cb23635ed448397cfbcca9fea0e0923dadba84
ahehead <nojoycrystal@gmail.com>
2025-05-17 00:02:45 +0900

    feat: APIキーの保存と取得ロジックを追加

--- src/main/features/file/conf.ts
+++ src/main/features/file/conf.ts
@@ -1,3 +1,4 @@
+import { safeStorage } from "electron";
 import { Conf } from "electron-conf";
 import {
   type ApplicationSettings,
@@ -5,7 +6,10 @@ import {
   createDefaultApplicationSettings,
 } from "main/types";
 import {
+  type ApiKeysFlags,
+  type ApiKeysSecrets,
   createPersistedApiKeysState,
+  secretsToFlags,
   type PersistedApiKeysState,
 } from "shared/ApiKeysType";
 
@@ -16,9 +20,37 @@ export function ApplicationSettingsConf(): Conf<ApplicationSettings> {
   });
 }
 
-export function ApiKeysConf(): Conf<PersistedApiKeysState> {
+export function ApiKeyConf(): Conf<PersistedApiKeysState> {
   return new Conf<PersistedApiKeysState>({
     name: ConfFileName.ApiKeys,
     defaults: createPersistedApiKeysState(),
   });
+}
+
+export function saveApiKeyConf(
+  apiKeysConf: ReturnType<typeof ApiKeyConf>,
+  service: keyof ApiKeysFlags,
+  apiKey: string
+): void {
+  apiKeysConf.delete(`keys.${service}`);
+  const buffer = safeStorage.encryptString(apiKey);
+  apiKeysConf.set(`keys.${service}`, buffer.toString("latin1"));
+}
+
+export function getApiKeyFlagsConf(
+  apiKeysConf: ReturnType<typeof ApiKeyConf>
+): ApiKeysFlags {
+  return secretsToFlags(apiKeysConf.get("keys") as ApiKeysSecrets);
+}
+
+export function getApiKeyConf(
+  apiKeysConf: ReturnType<typeof ApiKeyConf>,
+  service: keyof ApiKeysFlags
+): string {
+  const encrypted = apiKeysConf.get(`keys.${service}`) as string | null;
+  const apiKey = encrypted
+    ? safeStorage.decryptString(Buffer.from(encrypted, "latin1"))
+    : null;
+  if (!apiKey) throw new Error("APIキー未設定");
+  return apiKey;
 }
