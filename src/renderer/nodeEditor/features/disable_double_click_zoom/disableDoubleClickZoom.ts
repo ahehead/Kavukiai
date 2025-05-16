bba6202e857e036ffa2089eab8cb6f1bdc440f8e
ahehead <nojoycrystal@gmail.com>
2025-05-16 18:32:22 +0900

    feat: ダブルクリックズームを無効にする機能を移動

---
+++ src/renderer/nodeEditor/features/disable_double_click_zoom/disableDoubleClickZoom.ts
@@ -0,0 +1,9 @@
+import type { AreaExtra, Schemes } from "renderer/nodeEditor/types";
+import type { AreaPlugin } from "rete-area-plugin";
+
+export function disableDoubleClickZoom(area: AreaPlugin<Schemes, AreaExtra>) {
+  area.addPipe((context) => {
+    if (context.type === "zoom" && context.data.source === "dblclick") return;
+    return context;
+  });
+}
