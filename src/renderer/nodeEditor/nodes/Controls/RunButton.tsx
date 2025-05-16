5ce79f6f87d1cf29ab69cf6d94e2988578a5619d
ahehead <nojoycrystal@gmail.com>
2025-05-16 20:09:07 +0900

    feat: RunButtonControlにDrag.NoDragラッパーを追加し、ドラッグ操作を無効化

--- src/renderer/nodeEditor/nodes/Controls/RunButton.tsx
+++ src/renderer/nodeEditor/nodes/Controls/RunButton.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { JSX } from 'react';
 import { ClassicPreset } from 'rete';
+import { Drag } from "rete-react-plugin";
 
 // Run ボタン用コントロール
 export class RunButtonControl extends ClassicPreset.Control {
@@ -22,13 +23,15 @@ function RunButton(props: {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
 }): React.JSX.Element {
   return (
-    <button
-      className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white  w-full"
-      onClick={props.onClick}
-    >
-      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-background dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent w-full">
-        {props.label}
-      </span>
-    </button>
+    <Drag.NoDrag>
+      <button
+        className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white  w-full"
+        onClick={props.onClick}
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-background dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent w-full">
+          {props.label}
+        </span>
+      </button>
+    </Drag.NoDrag>
   )
 }
