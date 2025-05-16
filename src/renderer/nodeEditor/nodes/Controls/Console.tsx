7e73c07fc12c6d09e7aee69d75b1d25a0fe04c5d
ahehead <nojoycrystal@gmail.com>
2025-05-16 16:25:17 +0900

    feat: ConsoleControlコンポーネントを追加し、テキストエリアのスクロールとホイールイベントの無効化を実装

---
+++ src/renderer/nodeEditor/nodes/Controls/Console.tsx
@@ -0,0 +1,48 @@
+import type React from "react"
+import { useEffect, useRef, type JSX } from 'react';
+import type { AreaExtra, Schemes } from "renderer/nodeEditor/types";
+import { ClassicPreset } from 'rete';
+import type { AreaPlugin } from "rete-area-plugin";
+import { Drag } from "rete-react-plugin";
+
+// Run ボタン用コントロール
+export class ConsoleControl extends ClassicPreset.Control {
+  value = "";
+  constructor(private area: AreaPlugin<Schemes, AreaExtra>) {
+    super();
+  }
+  addValue: (addValue: string) => void = (addValue) => {
+    this.value += addValue;
+    this.area.update("control", this.id);
+  };
+}
+
+export function ConsoleControlView(props: { data: ConsoleControl }): JSX.Element {
+  return <Console value={props.data.value} />;
+}
+
+function Console(props: { value: string }): React.JSX.Element {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.scrollTop = textareaRef.current.scrollHeight;
+    }
+  }, [props.value]);
+
+  return (
+    <Drag.NoDrag>
+      <textarea
+        ref={textareaRef}
+        className="w-full h-full p-2 border-none resize-none text-sm text-gray-700 dark:text-gray-200 rounded-md bg-gray-100 overflow-auto"
+        value={props.value}
+        readOnly
+        rows={1}
+        onWheel={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+      />
+    </Drag.NoDrag>
+  );
+}
