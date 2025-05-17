9a6a88fdbf5a96bd13b6d7675956609e982217bf
ahehead <nojoycrystal@gmail.com>
2025-05-17 16:31:46 +0900

    feat: OpenAIResponseParamNodeをNodeバレルファイルに追加

--- src/renderer/nodeEditor/nodes/Node/index.ts
+++ src/renderer/nodeEditor/nodes/Node/index.ts
@@ -4,3 +4,4 @@ export { MultiLineStringNode } from "./MultiLineStringNode";
 export { RunNode } from "./RunNode";
 export { ViewStringNode } from "./ViewStringNode";
 export { OpenAINode } from "./OpenAI";
+export { OpenAIResponseParamNode } from "./OpenAIResponseParam";
