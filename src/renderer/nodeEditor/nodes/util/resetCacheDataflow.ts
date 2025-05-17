426ff10cacbd827726e3fa5029707c781a388754
ahehead <nojoycrystal@gmail.com>
2025-05-17 16:18:43 +0900

    feat: resetCacheDataflow関数を追加し、データフローのキャッシュをリセットする機能を実装

---
+++ src/renderer/nodeEditor/nodes/util/resetCacheDataflow.ts
@@ -0,0 +1,13 @@
+import type { Schemes } from "renderer/nodeEditor/types";
+import type { DataflowEngine } from "rete-engine";
+
+export function resetCacheDataflow(
+  dataflow: DataflowEngine<Schemes>,
+  id: string
+) {
+  try {
+    dataflow.reset(id);
+  } catch (error) {
+    console.info("No cache in the data flow. nodeId:", id);
+  }
+}
