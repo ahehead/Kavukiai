5dff9089ce925babb7b79d84fa988a0e8de93b2e
ahehead <nojoycrystal@gmail.com>
2025-05-18 05:31:33 +0900

    feat: ChatContextNodeクラスを新規作成し、長文文字列入力ノードを実装

---
+++ src/renderer/nodeEditor/nodes/Node/ChatContextNode.ts
@@ -0,0 +1,53 @@
+import { ClassicPreset } from "rete";
+import { createSocket } from "../Sockets";
+import type { HistoryPlugin } from "rete-history-plugin";
+import {
+  BaseNode,
+  type AreaExtra,
+  type CustomSocketType,
+  type Schemes,
+} from "renderer/nodeEditor/types";
+import type { AreaPlugin } from "rete-area-plugin";
+import type { DataflowEngine } from "rete-engine";
+import { resetCacheDataflow } from "../util/resetCacheDataflow";
+import {
+  type ChatContext,
+  ChatContextControl,
+} from "../Controls/ChatContext/ChatContext";
+const { Output } = ClassicPreset;
+
+// 長文文字列入力ノード
+export class ChatContextNode extends BaseNode<
+  object,
+  { out: CustomSocketType },
+  { chatContext: ChatContextControl }
+> {
+  constructor(
+    initial: ChatContext,
+    history: HistoryPlugin<Schemes>,
+    area: AreaPlugin<Schemes, AreaExtra>,
+    dataflow: DataflowEngine<Schemes>
+  ) {
+    super("ChatContext");
+    this.addOutput("out", new Output(createSocket("chatContext"), undefined));
+    this.addControl(
+      "chatContext",
+      new ChatContextControl(initial, {
+        editable: true,
+        history: history,
+        area: area,
+        onChange: (v: ChatContext) => {
+          resetCacheDataflow(dataflow, this.id);
+        },
+      })
+    );
+  }
+
+  // dataflowで流す
+  data(): { out: ChatContext } {
+    console.log("data", this.controls.chatContext.getContext());
+    return { out: this.controls.chatContext.getContext() || [] };
+  }
+
+  async execute(): Promise<void> {}
+}
