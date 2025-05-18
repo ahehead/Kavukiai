2d8d6b075cec2d42b178c7ef944d1287bf82ac70
ahehead <nojoycrystal@gmail.com>
2025-05-18 22:57:44 +0900

    fix: OpenAINodeとOpenAIResponseParamNodeのインポートパスを修正

--- src/renderer/nodeEditor/nodes/Node/index.ts
+++ src/renderer/nodeEditor/nodes/Node/index.ts
@@ -3,6 +3,6 @@ export { StringNode } from "./StringNode";
 export { MultiLineStringNode } from "./MultiLineStringNode";
 export { RunNode } from "./RunNode";
 export { ViewStringNode } from "./ViewStringNode";
-export { OpenAINode } from "./OpenAI";
-export { OpenAIResponseParamNode } from "./OpenAIResponseParam";
+export { OpenAINode } from "./OpenAI/OpenAI";
+export { OpenAIResponseParamNode } from "./OpenAI/OpenAIResponseParam";
 export { ChatContextNode } from "./ChatContextNode";
