a4601fbad7ae2363ac949e47c22e83c8f8e6c860
ahehead <nojoycrystal@gmail.com>
2025-05-18 23:49:27 +0900

    feat: customReactPresets関数を新規作成し、カスタムノードの設定を実装

---
+++ src/renderer/nodeEditor/features/customReactPresets/customReactPresets.ts
@@ -0,0 +1,74 @@
+import { type ReactArea2D, Presets as ReactPresets } from "rete-react-plugin";
+import type { AreaPlugin } from "rete-area-plugin";
+import type { HistoryPlugin } from "rete-history-plugin";
+import type { Schemes, AreaExtra } from "../../types";
+import {
+  RunButtonControl,
+  RunButtonControlView,
+} from "../../nodes/Controls/RunButton";
+import {
+  MultiLineControl,
+  TextAreaControllView,
+} from "../../nodes/Controls/TextArea";
+import {
+  ConsoleControl,
+  ConsoleControlView,
+} from "../../nodes/Controls/Console";
+import {
+  InputValueControl,
+  InputValueControlView,
+} from "../../nodes/Controls/InputValue";
+import {
+  ChatContextControl,
+  ChatContextControlView,
+} from "../../nodes/Controls/ChatContext/ChatContext";
+import {
+  CheckBoxControl,
+  CheckBoxControlView,
+} from "../../nodes/Controls/CheckBox";
+import {
+  CustomExecSocket,
+  CustomSocket,
+  createCustomNode,
+} from "../../component";
+
+export function customReactPresets(
+  area: AreaPlugin<Schemes, AreaExtra>,
+  history: HistoryPlugin<Schemes>,
+  getZoom: () => number
+) {
+  return ReactPresets.classic.setup<Schemes, ReactArea2D<Schemes>>({
+    customize: {
+      socket(data) {
+        if (data.payload.name === "exec") {
+          return CustomExecSocket;
+        }
+        return CustomSocket;
+      },
+      control(data) {
+        if (data.payload instanceof RunButtonControl) {
+          return RunButtonControlView;
+        }
+        if (data.payload instanceof MultiLineControl) {
+          return TextAreaControllView;
+        }
+        if (data.payload instanceof ConsoleControl) {
+          return ConsoleControlView;
+        }
+        if (data.payload instanceof InputValueControl) {
+          return InputValueControlView;
+        }
+        if (data.payload instanceof ChatContextControl) {
+          return ChatContextControlView;
+        }
+        if (data.payload instanceof CheckBoxControl) {
+          return CheckBoxControlView;
+        }
+        return null;
+      },
+      node() {
+        return createCustomNode(area, history, getZoom);
+      },
+    },
+  });
+}
