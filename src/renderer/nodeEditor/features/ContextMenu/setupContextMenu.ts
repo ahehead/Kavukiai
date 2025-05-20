ac0ec026c0378f1e0a3b9b4c0f11c9f1c19ad6b3
ahehead <nojoycrystal@gmail.com>
2025-05-20 23:26:21 +0900

    feat: 入力方法切替メニュー項目を追加し、接続削除機能を実装

--- src/renderer/nodeEditor/features/ContextMenu/setupContextMenu.ts
+++ src/renderer/nodeEditor/features/ContextMenu/setupContextMenu.ts
@@ -1,13 +1,23 @@
 import { ContextMenuPlugin } from "rete-context-menu-plugin";
-import type { NodeEditor } from "rete";
-import type { Schemes, AreaExtra } from "../../types";
+import type { ClassicPreset, NodeEditor } from "rete";
+import type {
+  Schemes,
+  AreaExtra,
+  NodeInterface,
+  CustomSocketType,
+} from "../../types";
 
 import { contextMenuStructure, type NodeDeps } from "../../nodes/nodeFactories";
-import { removeNodeWithConnections } from "../../nodes/util/removeNode";
+import {
+  removeLinkedSockets,
+  removeNodeWithConnections,
+} from "../../nodes/util/removeNode";
 import type { AreaPlugin } from "rete-area-plugin";
 import type { DataflowEngine, ControlFlowEngine } from "rete-engine";
 import type { HistoryPlugin, HistoryActions } from "rete-history-plugin";
 import { createReteContextMenuItems } from "./createContextMenu";
+import type { Item } from "rete-context-menu-plugin/_types/types";
+import { resetCacheDataflow } from "renderer/nodeEditor/nodes/util/resetCacheDataflow";
 
 type ContextMenuDependencies = {
   editor: NodeEditor<Schemes>;
@@ -17,6 +27,16 @@ type ContextMenuDependencies = {
   history: HistoryPlugin<Schemes, HistoryActions<Schemes>>;
 };
 
+function isConnection(
+  ctx: any
+): ctx is { id: string; source: string; target: string } {
+  return ctx && typeof ctx === "object" && "source" in ctx && "target" in ctx;
+}
+
+function isNode(ctx: any): ctx is NodeInterface {
+  return ctx && typeof ctx === "object" && "id" in ctx && "inputs" in ctx;
+}
+
 export function setupContextMenu({
   editor,
   area,
@@ -24,9 +44,11 @@ export function setupContextMenu({
   controlflow,
   history,
 }: ContextMenuDependencies) {
+  const nodeDeps: NodeDeps = { area, dataflow, controlflow, history };
+
   return new ContextMenuPlugin<Schemes>({
     items: (context) => {
-      const nodeDeps: NodeDeps = { area, dataflow, controlflow, history };
+      //console.log("context", context);
 
       if (context === "root") {
         return {
@@ -38,24 +60,103 @@ export function setupContextMenu({
           ),
         };
       }
+
+      const listItems: Item[] = [];
+
+      // node のinputにcontrolがある場合、showControlをtoggleするメニューを追加
+      if (isNode(context)) {
+        // 表示方式を変えられるinputのリスト
+        const inputlist = filterInputControls(context.inputs);
+        if (inputlist.length > 0) {
+          listItems.push(
+            createToggleInputControlMenuItem(
+              context,
+              editor,
+              area,
+              dataflow,
+              inputlist
+            )
+          );
+        }
+      }
+
+      if (isConnection(context)) {
+        listItems.push(deleteConnectionItem(context, editor));
+      }
+      // nodeとconnection削除機能
+      listItems.push(deleteNodeItem(context, editor));
       return {
         searchBar: false,
-        list: [
-          {
-            label: "Delete",
-            key: "delete",
-            async handler() {
-              if ("source" in context && "target" in context) {
-                // connection
-                await editor.removeConnection(context.id);
-              } else {
-                // node
-                await removeNodeWithConnections(editor, context.id);
-              }
-            },
-          },
-        ],
+        list: listItems,
       };
     },
   });
+}
+
+function deleteConnectionItem(
+  context: { id: string },
+  editor: NodeEditor<Schemes>
+) {
+  return {
+    label: "接続を削除",
+    key: "delete-connection",
+    async handler() {
+      await editor.removeConnection(context.id);
+    },
+  };
+}
+
+function deleteNodeItem(context: any, editor: NodeEditor<Schemes>) {
+  return {
+    label: "ノードを削除",
+    key: "delete-node",
+    async handler() {
+      await removeNodeWithConnections(editor, context.id);
+    },
+  };
+}
+
+// controlを持っているinputをキー付きでlistで返す
+function filterInputControls(
+  inputs: NodeInterface["inputs"]
+): Array<{ key: string; input: ClassicPreset.Input<CustomSocketType> }> {
+  const entries = Object.entries(inputs) as [
+    string,
+    ClassicPreset.Input<CustomSocketType> | undefined
+  ][];
+  const filtered = entries.filter(
+    (entry): entry is [string, ClassicPreset.Input<CustomSocketType>] => {
+      const input = entry[1];
+      return input?.control != null;
+    }
+  );
+  return filtered.map(([key, input]) => ({ key, input }));
+}
+
+export function createToggleInputControlMenuItem(
+  context: NodeInterface,
+  editor: NodeEditor<Schemes>,
+  area: AreaPlugin<Schemes, AreaExtra>,
+  dataflow: DataflowEngine<Schemes>,
+  inputlist: Array<{
+    key: string;
+    input: ClassicPreset.Input<CustomSocketType>;
+  }>
+) {
+  return {
+    label: "入力方法切替",
+    key: "control-style",
+    handler: () => void 0,
+    subitems: inputlist.map(({ key, input }) => ({
+      label: `${key}`,
+      key: `${context.id}_${input.id}`,
+      async handler() {
+        input.showControl = !input.showControl;
+        await removeLinkedSockets(editor, context.id, key);
+        resetCacheDataflow(dataflow, context.id);
+        await area.update("node", context.id);
+        console.log(`Toggled showControl for ${key} to ${input.showControl}`);
+      },
+    })),
+  };
 }
