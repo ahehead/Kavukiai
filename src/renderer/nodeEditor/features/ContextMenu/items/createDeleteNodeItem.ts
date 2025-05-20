8149960e8221c6763b34b823993584f09624c124
ahehead <nojoycrystal@gmail.com>
2025-05-21 01:15:10 +0900

    feat: ノード削除用のコンテキストメニュー項目を新規作成

---
+++ src/renderer/nodeEditor/features/ContextMenu/items/createDeleteNodeItem.ts
@@ -0,0 +1,17 @@
+import type { NodeEditor } from "rete";
+import type { Schemes } from "../../../types";
+import { removeNodeWithConnections } from "../../../nodes/util/removeNode";
+import type { Item } from "rete-context-menu-plugin/_types/types";
+
+export function createDeleteNodeItem(
+  context: { id: string },
+  editor: NodeEditor<Schemes>
+): Item {
+  return {
+    label: "ノードを削除",
+    key: "delete-node",
+    async handler() {
+      await removeNodeWithConnections(editor, context.id);
+    },
+  };
+}
