5927cccf30a5dbda045c4cf9a9678cc60b6f8b7d
ahehead <nojoycrystal@gmail.com>
2025-05-22 05:43:31 +0900

    feat: 入力コントロールのラッパーコンポーネントとスタイルを追加

--- src/renderer/components/NodePanel.tsx
+++ src/renderer/components/NodePanel.tsx
@@ -61,32 +61,7 @@ export function NodeSocketsWrapper({ ...props }: React.ComponentProps<"div">) {
 }
 
 
-export const textAreaStyles = cva(
-  ["w-full h-full py-2 px-2.5 resize-none border-none rounded-md"],
-  {
-    variants: {
-      editable: {
-        true: " ring-1 ring-input focus:ring-2 focus:ring-accent",
-        false: " bg-gray-100 ring-0 focus:ring-0 outline-none focus:outline-none",
-      },
-    },
-    defaultVariants: {
-      editable: true,
-    },
-  }
-);
 
-export const inputValueStyles = cva(
-  ["w-full min-w-0 rounded-full border border-input bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"],
-  {
-    variants: {
-      editable: {
-        true: "",
-        false: "bg-gray-100 cursor-not-allowed",
-      }
-    }
-  }
-);
 
 
 
@@ -195,3 +170,50 @@ export function NodeControlsWrapper({ ...props }: React.ComponentProps<"div">) {
     <div className='w-full h-full p-2 empty:p-0' {...props} />
   )
 }
+
+export const textAreaStyles = cva(
+  ["w-full h-full py-2 px-2.5 resize-none border-none rounded-md"],
+  {
+    variants: {
+      editable: {
+        true: " ring-1 ring-input focus:ring-2 focus:ring-accent",
+        false: " bg-gray-100 ring-0 focus:ring-0 outline-none focus:outline-none",
+      },
+    },
+    defaultVariants: {
+      editable: true,
+    },
+  }
+);
+
+
+
+export function InputControlWrapper({ ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={"grid grid-cols-1 w-full"}
+      {...props}
+    />
+  )
+}
+
+export const inputControlLabelStyles = cva(
+  "block w-full truncate text-xs text-gray-500 mb-1.5"
+)
+
+export function InputControlLabel({ children, ...props }: React.ComponentProps<"label">) {
+  // biome-ignore lint/a11y/noLabelWithoutControl: <explanation>
+  return <label {...props} className={inputControlLabelStyles()}>{children}</label>;
+}
+
+export const inputValueStyles = cva(
+  ["w-full min-w-0 rounded-full border border-input bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"],
+  {
+    variants: {
+      editable: {
+        true: "",
+        false: "bg-gray-100 cursor-not-allowed",
+      }
+    }
+  }
+);
