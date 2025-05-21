cd55e5d93c100b705e89a3b290e37f24ee1aaae9
ahehead <nojoycrystal@gmail.com>
2025-05-22 00:58:05 +0900

    feat: TestNodeクラスを追加し、基本的な出力と制御を実装

---
+++ src/renderer/nodeEditor/nodes/Node/TestNode.tsx
@@ -0,0 +1,31 @@
+import { ClassicPreset } from 'rete';
+import type { ControlFlowEngine } from 'rete-engine';
+import { BaseNode } from "renderer/nodeEditor/types/BaseNode";
+import { createSocket, type NodeSocket, type Schemes } from 'renderer/nodeEditor/types';
+import { CheckBoxControl } from '../Controls/CheckBox';
+const { Output } = ClassicPreset;
+
+// src/renderer/nodeEditor/features/customReactPresets/customReactPresets.ts
+// 型チェック回避用のNode…使うことはない。
+export class TestNode extends BaseNode<
+  object,
+  { exec: NodeSocket },
+  { check: CheckBoxControl }
+> {
+  constructor(
+    private engine: ControlFlowEngine<Schemes>
+  ) {
+    super('Test');
+    this.addOutput('exec', new Output(createSocket("exec"), undefined, true));
+    this.addControl(
+      'check',
+      new CheckBoxControl(true, { label: 'CheckBox' })
+    );
+  }
+
+  data(): object { return {} }
+
+  async execute(_: never, forward: (output: 'exec') => void): Promise<void> {
+    forward('exec');
+  }
+}
