0d93786c69ad5ec271ee03f43e8c968a7441117d
ahehead <nojoycrystal@gmail.com>
2025-05-23 01:45:51 +0900

    feat: SerializableInputsNodeクラスを追加し、入力のシリアライズ機能を実装

--- src/renderer/nodeEditor/types/BaseNode.ts
+++ src/renderer/nodeEditor/types/BaseNode.ts
@@ -1,6 +1,7 @@
 import { ClassicPreset } from "rete";
 import type { NodeSocket } from "./NodeSocket";
-import type { NodeControl } from "./NodeControl";
+import type { NodeControl, SerializableControl } from "./NodeControl";
+import type { InputPortJson } from "shared/JsonType";
 
 // ClassicPreset.Nodeを拡張
 export class BaseNode<
@@ -29,5 +30,49 @@ export class BaseNode<
   }
   clearHeight() {
     this.height = undefined;
+  }
+}
+
+// inputsをシリアライズできるように拡張したノードクラス
+export class SerializableInputsNode<
+  Inputs extends { [key in string]?: NodeSocket },
+  Outputs extends { [key in string]?: NodeSocket },
+  Controls extends { [key in string]?: NodeControl }
+> extends BaseNode<Inputs, Outputs, Controls> {
+  protected hasToJson = (c: unknown): c is SerializableControl =>
+    typeof (c as SerializableControl | undefined)?.toJSON === "function";
+
+  protected hasSetFromJson = (c: unknown): c is SerializableControl =>
+    typeof (c as any).setFromJSON === "function";
+
+  toInputsJson(): { inputs: Record<string, InputPortJson> } {
+    const inputsJson: Record<string, InputPortJson> = {};
+    for (const [key, input] of Object.entries(this.inputs)) {
+      if (input) {
+        inputsJson[key] = {
+          isShowControl: input.showControl,
+          ...(this.hasToJson(input.control) && {
+            control: input.control.toJSON(),
+          }),
+        };
+      }
+    }
+    return { inputs: inputsJson };
+  }
+
+  setFromInputsJson(inputsJson: Record<string, InputPortJson>): void {
+    for (const [key, inputJson] of Object.entries(inputsJson)) {
+      const input = this.inputs[key as keyof Inputs];
+      if (input) {
+        input.showControl = inputJson.isShowControl;
+        if (
+          inputJson.control &&
+          input.control &&
+          this.hasSetFromJson(input.control)
+        ) {
+          input.control.setFromJSON(inputJson.control);
+        }
+      }
+    }
   }
 }
