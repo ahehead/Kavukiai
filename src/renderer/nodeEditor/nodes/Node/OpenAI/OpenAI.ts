5d0210c5bb1c70a29b35bf9ee046b532c8ff6b16
ahehead <nojoycrystal@gmail.com>
2025-05-23 09:28:27 +0900

    feat: OpenAINodeにポートのクローズ処理と終了処理を追加

--- src/renderer/nodeEditor/nodes/Node/OpenAI/OpenAI.ts
+++ src/renderer/nodeEditor/nodes/Node/OpenAI/OpenAI.ts
@@ -65,49 +65,78 @@ export class OpenAINode extends SerializableInputsNode<
     this.value += value;
     resetCacheDataflow(this.dataflow, this.id);
   }
+
   setString(value: string): void {
     this.value = value;
     resetCacheDataflow(this.dataflow, this.id);
   }
 
+  resetString(): void {
+    this.value = "";
+    resetCacheDataflow(this.dataflow, this.id);
+  }
+  // portのclose処理
+  closePort(): void {
+    if (this.port) {
+      this.port.close();
+      this.port = null;
+    }
+  }
+
+  //errorとdoneのときの終了処理
+  logAndTerminate(type: "error" | "done", message: string): void {
+    if (type === "error") {
+      this.controls.console.addValue(`Error: ${message}`);
+      console.error(`Error: ${message}`);
+      this.setStatus(this.area, NodeStatus.ERROR);
+    } else if (type === "done") {
+      this.controls.console.addValue(`Done: ${message}`);
+      console.log(`Done: ${message}`);
+      this.setStatus(this.area, NodeStatus.COMPLETED);
+    }
+    this.closePort();
+  }
+
   data(): object {
     return { message: this.value };
   }
+
   async execute(
     input: "exec" | "exec2",
     forward: (output: "exec") => void
   ): Promise<void> {
     // exec2が実行された場合は、ポートを閉じて終了
-    if (input === "exec2" && this.port) {
-      const message: PortEventType = { type: "abort" };
-      this.port.postMessage(message);
-      this.port.close();
-      this.port = null;
-      this.setStatus(this.area, NodeStatus.IDLE);
-      this.controls.console.addValue("stop");
+    if (input === "exec2") {
+      if (this.status === NodeStatus.RUNNING && this.port) {
+        const message: PortEventType = { type: "abort" };
+        this.port.postMessage(message);
+        this.closePort();
+        this.setStatus(this.area, NodeStatus.IDLE);
+        this.controls.console.addValue("Stop");
+      } else {
+        this.controls.console.addValue("Already stopped");
+      }
       return;
     }
 
-    // idleとcomplete以外の状態で実行された場合は何もしない
-    if (
-      !(this.status === NodeStatus.IDLE || this.status === NodeStatus.COMPLETED)
-    ) {
-      this.controls.console.addValue("Info: Already running");
+    // running状態で実行された場合は何もしない
+    if (this.status === NodeStatus.RUNNING) {
+      this.controls.console.addValue("Already running");
       return;
     }
+
     this.setStatus(this.area, NodeStatus.RUNNING);
-    this.setString("");
+    this.resetString();
     const { param } = (await this.dataflow.fetchInputs(this.id)) as {
       param?: OpenAI.Responses.ResponseCreateParams[];
     };
-    this.controls.console.addValue(`param: ${JSON.stringify(param, null, 2)}`);
+    this.controls.console.addValue(`Param: ${JSON.stringify(param, null, 2)}`);
     if (!param) {
-      this.controls.console.addValue("Error: No param");
-      console.error("Error: No param");
-      this.setStatus(this.area, NodeStatus.ERROR);
+      this.logAndTerminate("error", "No param");
       return;
     }
-    this.controls.console.addValue("start");
+    this.controls.console.addValue("Start");
+    // ポートの作成とstart
     this.port = await createOpenAIMessagePort({
       id: this.id,
       param: param[0],
@@ -117,22 +146,14 @@ export class OpenAINode extends SerializableInputsNode<
       const result = e.data as PortEventType;
       //console.log("result", result);
       if (result.type === "error") {
-        this.controls.console.addValue(`Error: ${result.message}`);
-        console.error("Error:", result.message);
-        this.setStatus(this.area, NodeStatus.ERROR);
-        this.port?.close();
-        this.port = null;
+        this.logAndTerminate("error", result.message);
       }
       if (result.type === "delta") {
         this.addString(result.value);
       }
       if (result.type === "done") {
         this.setString(result.text);
-        console.log("done", result);
-        this.controls.console.addValue("done");
-        this.port?.close();
-        this.port = null;
-        this.setStatus(this.area, NodeStatus.COMPLETED);
+        this.logAndTerminate("done", result.text);
       }
       forward("exec");
     };
