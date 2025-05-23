2a2f6273e906329b20489b6c8a3d30476014e251
ahehead <nojoycrystal@gmail.com>
2025-05-23 21:10:46 +0900

    feat: ViewStringNodeの入力および出力の型を一般化し、柔軟性を向上

--- src/renderer/nodeEditor/nodes/Node/ViewStringNode.tsx
+++ src/renderer/nodeEditor/nodes/Node/ViewStringNode.tsx
@@ -9,8 +9,8 @@ const { Input, Output } = ClassicPreset;
 
 // View String ノード
 export class ViewStringNode extends BaseNode<
-  { exec: NodeSocket; inputString: NodeSocket },
-  { exec: NodeSocket; outputString: NodeSocket },
+  { exec: NodeSocket; inputAny: NodeSocket },
+  { exec: NodeSocket; outputAny: NodeSocket },
   { view: MultiLineControl }
 > {
   constructor(
@@ -23,34 +23,34 @@ export class ViewStringNode extends BaseNode<
       'exec',
       new Input(createSocket("exec"), undefined, false));
     this.addInput(
-      'inputString',
-      new Input(createSocket("string"), undefined, false));
+      'inputAny',
+      new Input(createSocket("any"), undefined, false));
 
     this.addOutput(
       'exec',
       new Output(createSocket("exec"), undefined, true));
     this.addOutput(
-      'outputString',
-      new Output(createSocket("string"), undefined,));
+      'outputAny',
+      new Output(createSocket("any"), undefined, true));
 
     this.addControl(
       'view',
       new MultiLineControl("", { editable: false, }));
   }
 
-  data(inputs: { inputString?: string[] }): { outputString: string } {
-    const value = inputs.inputString?.[0] || '';
-    return { outputString: value };
+  data(inputs: { inputAny?: any[] }): { outputAny: string } {
+    const value = inputs.inputAny?.[0] || '';
+    return { outputAny: value };
   }
 
   // 実行時、inputを取得して表示する
   async execute(_input: 'exec', forward: (output: 'exec') => void): Promise<void> {
-    const { inputString } = (await this.dataflow.fetchInputs(this.id)) as { inputString?: string[] }
+    const { inputAny } = (await this.dataflow.fetchInputs(this.id)) as { inputAny?: any[] }
 
     // inputがundefinedの場合は何もしない
-    if (!inputString) return;
+    if (!inputAny) return;
 
-    this.controls.view.setValue(inputString[0] || '');
+    this.controls.view.setValue(`${inputAny[0]}` || '');
     resetCacheDataflow(this.dataflow, this.id);
     await this.area.update("control", this.controls.view.id);
 
