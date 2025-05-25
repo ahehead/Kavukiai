139e60e1e3d5ea11bbf036f1b0347235a6f80329
ahehead <nojoycrystal@gmail.com>
2025-05-25 16:14:01 +0900

    feat: customContextMenuPresetを新規作成し、カスタムコンテキストメニューをレンダリング

---
+++ src/renderer/nodeEditor/features/customReactPresets/customContextMenuPreset.tsx
@@ -0,0 +1,22 @@
+import type { BaseSchemes } from "rete";
+import type { ContextMenuRender } from "rete-react-plugin/_types/presets/context-menu/types";
+import type { RenderPreset } from "rete-react-plugin/_types/presets/types";
+import { CustomContextMenu } from "../customization/CustomContextMenu";
+
+export function customContextMenuPreset<S extends BaseSchemes>(): RenderPreset<S, ContextMenuRender> {
+  return {
+    render(ctx: ContextMenuRender) {
+      if (ctx.data.type === "contextmenu") {
+        return (
+          <CustomContextMenu
+            element={ctx.data.element}
+            type={ctx.data.type}
+            items={ctx.data.items}
+            searchBar={ctx.data.searchBar}
+            onHide={ctx.data.onHide}
+          />
+        );
+      }
+    },
+  };
+}
