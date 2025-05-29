5dff69e96026522c4e145a911dfa4fc09f7ba684
ahehead <nojoycrystal@gmail.com>
2025-05-29 21:51:00 +0900

    feat: ControlOptionsインターフェースを追加し、BaseControlクラスのオプションを整理

--- src/renderer/nodeEditor/types/NodeControl.ts
+++ src/renderer/nodeEditor/types/NodeControl.ts
@@ -8,6 +8,10 @@ import type { MultiLineControl } from "../nodes/Controls/TextArea";
 import type { ButtonControl } from "../nodes/Controls/Button";
 import type { SelectControl } from "../nodes/Controls/Select";
 import { ClassicPreset } from "rete";
+import type { HistoryPlugin } from "rete-history-plugin";
+import type { AreaExtra, Schemes } from ".";
+import type { AreaPlugin } from "rete-area-plugin";
+import { ValueHistoryAction } from "./ValueHistoryAction";
 
 export type NodeControl =
   | RunButtonControl
@@ -25,12 +29,68 @@ export interface SerializableControl {
   setFromJSON({ data }: ControlJson): void;
 }
 
-export abstract class BaseControl
+export interface ControlOptions<T> {
+  label?: string;
+  editable?: boolean;
+  history?: HistoryPlugin<Schemes>;
+  area?: AreaPlugin<Schemes, AreaExtra>;
+  onChange?: (v: T) => void;
+}
+
+export abstract class BaseControl<T, O extends ControlOptions<T>>
   extends ClassicPreset.Control
   implements SerializableControl
 {
+  opts: ControlOptions<T>;
+
+  constructor(opts: O = {} as O) {
+    super();
+    this.opts = { editable: true, ...opts };
+  }
+
+  abstract setValue(value: any): void;
+
+  setLabel(label: string) {
+    this.opts.label = label;
+  }
+  getLabel(): string | undefined {
+    return this.opts.label;
+  }
+  setEditable(editable: boolean) {
+    this.opts.editable = editable;
+  }
+  getEditable(): boolean {
+    return this.opts.editable ?? true;
+  }
+  setHistory(history: HistoryPlugin<Schemes> | undefined) {
+    this.opts.history = history;
+  }
+  getHistory(): HistoryPlugin<Schemes> | undefined {
+    return this.opts.history;
+  }
+  setArea(area: AreaPlugin<Schemes, AreaExtra> | undefined) {
+    this.opts.area = area;
+  }
+  getArea(): AreaPlugin<Schemes, AreaExtra> | undefined {
+    return this.opts.area;
+  }
+  setOnChange(onChange: (v: T) => void) {
+    this.opts.onChange = onChange;
+  }
+  getOnChange(): ((v: T) => void) | undefined {
+    return this.opts.onChange;
+  }
+
   toJSON(): ControlJson | undefined {
     return undefined;
   }
   setFromJSON({ data }: ControlJson): void {}
+
+  addHistory(prev: T, next: T) {
+    if (this.opts.history && this.opts.area) {
+      this.opts.history.add(
+        new ValueHistoryAction(this, this.opts.area, prev, next)
+      );
+    }
+  }
 }
