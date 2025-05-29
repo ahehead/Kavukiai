bb90d6d11a9b1380607fb9ca781dec2a9804f6d3
ahehead <nojoycrystal@gmail.com>
2025-05-30 01:33:24 +0900

    refactor: Buttonコンポーネント内のボタンをdivでラップ

--- src/renderer/nodeEditor/nodes/Controls/Button.tsx
+++ src/renderer/nodeEditor/nodes/Controls/Button.tsx
@@ -37,12 +37,14 @@ function Button(props: {
 }): React.JSX.Element {
   return (
     <Drag.NoDrag>
-      <button
-        className="flex items-center justify-center p-1 overflow-hidden text-sm font-medium w-full rounded-lg border-border border-1 hover:bg-accent/50 text-foreground active:bg-accent/90 bg-node-bg"
-        onClick={props.onClick}
-      >
-        {props.label}
-      </button>
+      <div className="px-2">
+        <button
+          className="flex items-center justify-center p-1 overflow-hidden text-sm font-medium w-full rounded-lg border-border border-1 hover:bg-accent/50 text-foreground active:bg-accent/90 bg-node-bg"
+          onClick={props.onClick}
+        >
+          {props.label}
+        </button>
+      </div>
     </Drag.NoDrag>
   )
 }
