669d0a876a5a17fd1b98b07cef4c19b7a2e91c64
ahehead <nojoycrystal@gmail.com>
2025-05-31 15:27:05 +0900

    refactor: NodePortをNodeInputPortおよびNodeOutputPortに置き換え、入力と出力の処理を改善

--- src/renderer/nodeEditor/features/customization/CustomBaseNode.tsx
+++ src/renderer/nodeEditor/features/customization/CustomBaseNode.tsx
@@ -1,13 +1,14 @@
 import { Presets, type RenderEmit } from 'rete-react-plugin'
 import type { AreaExtra, NodeInterface, Schemes } from '../../types/Schemes'
 import { NodeContainer, NodeHeader, NodeTitle, NodeControlsWrapper, } from 'renderer/nodeEditor/component/nodeParts/NodePanel'
-import { NodeSocketName, NodeSocketTypeLabel } from "renderer/nodeEditor/component/nodeParts/NodeSocketParts"
-import { NodePort } from "renderer/nodeEditor/component/nodeParts/NodeSocketParts"
+import { NodeOutputPort, NodeSocketName, NodeSocketTypeLabel } from "renderer/nodeEditor/component/nodeParts/NodeSocketParts"
+import { NodeInputPort } from "renderer/nodeEditor/component/nodeParts/NodeSocketParts"
 import { NodeSocketsWrapper } from "renderer/nodeEditor/component/nodeParts/NodeSocketParts"
 import type { AreaPlugin } from 'rete-area-plugin'
 import { useRef } from 'react'
 import { useNodeResize } from '../../hooks/useNodeResize'
 import type { HistoryPlugin } from 'rete-history-plugin'
+import { ControlLabel } from 'renderer/nodeEditor/component/nodeParts/NodeControlParts'
 
 type Props<S extends Schemes> = {
   data: NodeInterface
@@ -63,10 +64,10 @@ export function createCustomNode(
           {/* Outputs */}
           {outputs.map(([key, output]) =>
             output && (
-              <NodePort
+              <NodeOutputPort
                 key={key}
                 data-testid={`output-${key}`}
-                side="output">
+              >
                 <NodeSocketName data-testid="output-title">
                   {output.label}
                 </NodeSocketName>
@@ -82,50 +83,66 @@ export function createCustomNode(
                   payload={output.socket}
                   data-testid="output-socket"
                 />
-              </NodePort>
+              </NodeOutputPort>
             )
           )}
 
           {/* Inputs */}
           {inputs.map(([key, input]) => {
             if (!input) return null;
-            if (!(input.control && input.showControl)) {
-              return (
-                <NodePort
-                  side="input"
-                  isShowAndHaveControl={input.control && input.showControl}
-                  key={key}
-                  data-testid={`input-${key}`}
-                >
-                  <Presets.classic.RefSocket
-                    name="input-socket"
-                    side="input"
-                    socketKey={key}
-                    nodeId={id}
-                    emit={emit}
-                    payload={input.socket}
-                    data-testid="input-socket"
-                  />
-                  <NodeSocketTypeLabel data-testid="input-type">
-                    {input.socket.name}
-                  </NodeSocketTypeLabel>
-                  <NodeSocketName data-testid="input-title">
-                    {input.label}
-                  </NodeSocketName>
-                </NodePort>
-              )
-            }
-            return (
-              <Presets.classic.RefControl
-                key={key}
-                name="input-control"
-                emit={emit}
-                payload={input.control}
-                data-testid="input-control"
-              />
 
+            return (
+              <NodeInputPort
+                showControl={Boolean(input.control && input.showControl)}
+                cols={input.control?.opts.cols || 0}
+                key={key}
+                data-testid={`input-${key}`}
+              >
+                {/* !showControl */}
+                {!(input.control && input.showControl) && (
+                  <>
+                    <Presets.classic.RefSocket
+                      name="input-socket"
+                      side="input"
+                      socketKey={key}
+                      nodeId={id}
+                      emit={emit}
+                      payload={input.socket}
+                      data-testid="input-socket"
+                    />
+                    <NodeSocketTypeLabel data-testid="input-type">
+                      {input.socket.name}
+                    </NodeSocketTypeLabel>
+                    <NodeSocketName data-testid="input-title">
+                      {input.label}
+                    </NodeSocketName>
+                  </>
+                )
+                }
+                {input.control && input.showControl && (
+                  <>
+                    {input.control.opts.label && (
+                      <ControlLabel
+                        cols={input.control.opts.cols}
+                        htmlFor={input.control.id}
+                      >
+                        {input.control.opts.label}
+                      </ControlLabel>
+                    )}
+                    <Presets.classic.RefControl
+                      key={key}
+                      name="input-control"
+                      emit={emit}
+                      payload={input.control}
+                      data-testid="input-control"
+                    />
+                  </>
+                )}
+              </NodeInputPort>
             )
           }
+
+
           )}
 
         </NodeSocketsWrapper>
