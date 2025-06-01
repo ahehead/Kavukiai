9f28438a65b92e2b753888937a4025abd619b658
ahehead <nojoycrystal@gmail.com>
2025-06-01 20:38:08 +0900

    feat: ツールチップのコンテンツにアラインメントを追加し、スタイルを調整

--- src/renderer/components/ui/tooltip.tsx
+++ src/renderer/components/ui/tooltip.tsx
@@ -1,4 +1,3 @@
-
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 import { cn } from "renderer/lib/utils"
 
@@ -43,8 +42,9 @@ function TooltipContent({
       <TooltipPrimitive.Content
         data-slot="tooltip-content"
         sideOffset={sideOffset}
+        align="center"
         className={cn(
-          "border border-node-border bg-node-bg text-node-fg animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) rounded-md px-3 py-1.5 text-xs text-balance",
+          "border border-node-border bg-node-bg text-node-fg animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-w-[300px] max-h-[300px] overflow-auto whitespace-nowrap origin-(--radix-tooltip-content-transform-origin) rounded-md px-3 py-1.5 text-xs text-balance tooltip-scrollbar",
           className
         )}
         {...props}
