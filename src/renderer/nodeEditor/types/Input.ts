8dedea96cfef7aedbc6b9645d8e10ed6c69d560d
ahehead <nojoycrystal@gmail.com>
2025-06-02 01:04:13 +0900

    feat: TooltipInputにrequireプロパティを追加し、コンストラクタで初期化できるように変更

--- src/renderer/nodeEditor/types/Input.ts
+++ src/renderer/nodeEditor/types/Input.ts
@@ -6,17 +6,20 @@ export class TooltipInput<
   S extends TypedSocket
 > extends ClassicPreset.Input<S> {
   tooltip?: string;
+  require?: boolean;
   override control: NodeControl | null;
 
   constructor(
     socket: S,
     label?: string,
     multipleConnections?: boolean,
-    tooltip?: string
+    tooltip?: string,
+    require?: boolean
   ) {
     super(socket, label, multipleConnections);
     this.tooltip = tooltip;
     this.control = null;
+    this.require = require;
   }
 
   setSocket(socket: S) {
