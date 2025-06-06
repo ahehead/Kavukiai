33d5ca6bb4ed6135735133bd6e04275647cdf6e8
ahehead <nojoycrystal@gmail.com>
2025-06-07 02:04:57 +0900

    fix: withTooltip関数のツールチップコンテンツをScrollAreaでラップし、可読性を向上

--- src/renderer/nodeEditor/features/customization/util.tsx
+++ src/renderer/nodeEditor/features/customization/util.tsx
@@ -1,20 +1,38 @@
 
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { ScrollArea } from "renderer/components/ui/scroll-area";
 import { Tooltip, TooltipContent, TooltipTrigger } from "renderer/components/ui/tooltip";
 import { Drag } from "rete-react-plugin";
 
 
 export function withTooltip(
   trigger: React.ReactElement,
+  isTypeLabel: boolean,
   tooltipText?: string
 ) {
   return tooltipText ? (
     <Tooltip>
       <TooltipTrigger asChild>{trigger}</TooltipTrigger>
-      <TooltipContent className="prose prose-sm">
+      <TooltipContent className={isTypeLabel ? "" : "prose not-prose-p:py-0"} variant={isTypeLabel ? "header" : "default"}>
         <Drag.NoDrag>
-          <Markdown remarkPlugins={[remarkGfm]}>{tooltipText}</Markdown>
+          <ScrollArea className="max-h-[90px] max-w-[300px]">
+            <Markdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ node, ...props }) {
+                  // コードブロック
+                  return (
+                    <pre className="not-prose">
+                      <code {...props} />
+                    </pre>
+                  );
+                },
+              }}
+            >
+              {tooltipText}
+            </Markdown>
+          </ScrollArea>
         </Drag.NoDrag>
       </TooltipContent>
     </Tooltip>
