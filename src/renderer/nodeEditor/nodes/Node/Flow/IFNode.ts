352501d5eefa565da967d679e7f5f996b42c7ad9
ahehead <nojoycrystal@gmail.com>
2025-06-11 21:58:15 +0900

    feat: IFNodeクラスを追加し、条件に基づく出力ポートを実装

---
+++ src/renderer/nodeEditor/nodes/Node/Flow/IFNode.ts
@@ -0,0 +1,79 @@
+import type { DataflowEngine } from "rete-engine";
+import { BaseNode } from "renderer/nodeEditor/types/Node/BaseNode";
+import type {
+  TypedSocket,
+  Schemes,
+  AreaExtra,
+} from "renderer/nodeEditor/types";
+import { Type } from "@sinclair/typebox";
+import { SwitchControl } from "../../Controls/input/Switch";
+import type { HistoryPlugin } from "rete-history-plugin";
+import type { AreaPlugin } from "rete-area-plugin";
+import { resetCacheDataflow } from "../../util/resetCacheDataflow";
+
+// IF ノード
+export class IFNode extends BaseNode<
+  { exec: TypedSocket; boolData: TypedSocket },
+  { exec: TypedSocket; exec2: TypedSocket },
+  object
+> {
+  constructor(
+    history: HistoryPlugin<Schemes>,
+    area: AreaPlugin<Schemes, AreaExtra>,
+    private dataflow: DataflowEngine<Schemes>
+  ) {
+    super("IF");
+    this.addInputPort([
+      {
+        key: "exec",
+        name: "exec",
+        label: "In",
+        schema: Type.Literal("exec"),
+      },
+      {
+        key: "boolData",
+        name: "boolean",
+        schema: Type.Boolean(),
+        label: "Condition",
+        control: new SwitchControl({
+          value: true,
+          history,
+          area,
+          onChange: () => resetCacheDataflow(dataflow, this.id),
+        }),
+      },
+    ]);
+    this.addOutputPort([
+      {
+        key: "exec",
+        name: "exec",
+        label: "True",
+        schema: Type.Literal("exec"),
+      },
+      {
+        key: "exec2",
+        name: "exec",
+        label: "False",
+        schema: Type.Literal("exec"),
+      },
+    ]);
+  }
+
+  data(): object {
+    return {};
+  }
+
+  async execute(
+    _: never,
+    forward: (output: "exec" | "exec2") => void
+  ): Promise<void> {
+    const { boolData } = (await this.dataflow.fetchInputs(this.id)) as {
+      boolData?: boolean[];
+    };
+    if (boolData?.[0]) {
+      forward("exec");
+      return;
+    }
+    forward("exec2");
+  }
+}
