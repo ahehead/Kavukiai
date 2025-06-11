44e4735272a3a44bda35bfe2473e1bbcfacdbc95
ahehead <nojoycrystal@gmail.com>
2025-06-11 21:53:14 +0900

    refactor: ConnectionParams型を定義し、onConnectionChangedSchemaのパラメータを簡素化

--- src/renderer/nodeEditor/types/Node/DynamicSchemaNode.ts
+++ src/renderer/nodeEditor/types/Node/DynamicSchemaNode.ts
@@ -1,9 +1,11 @@
 import type { TypedSocket } from "../TypedSocket";
 
+export type ConnectionParams = {
+  isConnected: boolean; // 接続状態
+  source: TypedSocket; // 接続元ソケット
+  target: TypedSocket; // 接続先ソケット
+};
+
 export interface DynamicSchemaNode {
-  onConnectionChangedSchema: (params: {
-    isConnected: boolean;
-    source: TypedSocket;
-    target: TypedSocket;
-  }) => Promise<void>;
+  onConnectionChangedSchema: (params: ConnectionParams) => Promise<void>;
 }
