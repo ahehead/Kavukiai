7ffeffe817f1feb8d57cdbad265ecbea8e9d5221
ahehead <nojoycrystal@gmail.com>
2025-06-13 23:56:58 +0900

    refactor: enhance ChatContextNode structure and input/output ports

--- src/renderer/nodeEditor/nodes/Node/OpenAI/ChatContextNode.ts
+++ src/renderer/nodeEditor/nodes/Node/OpenAI/ChatContextNode.ts
@@ -13,26 +13,67 @@ import {
   chatMessagesToResponseInput,
   ResponseInput,
 } from "renderer/nodeEditor/types/Schemas/InputSchemas";
-import type { ChatMessageItem } from "renderer/nodeEditor/types/Schemas/InputSchemas";
+import { ChatMessageItem } from "renderer/nodeEditor/types/Schemas/InputSchemas";
+import { Type } from "@sinclair/typebox";
 
 // 長文文字列入力ノード
 export class ChatContextNode extends BaseNode<
-  object,
-  { out: TypedSocket },
+  {
+    exec: TypedSocket;
+    exec2: TypedSocket;
+    systemPrompt: TypedSocket;
+    newMessage: TypedSocket;
+  },
+  { exec: TypedSocket; out: TypedSocket },
   { chatContext: ResponseInputMessageControl }
 > {
   constructor(
     initial: ChatMessageItem[],
     history: HistoryPlugin<Schemes>,
     area: AreaPlugin<Schemes, AreaExtra>,
-    dataflow: DataflowEngine<Schemes>
+    private dataflow: DataflowEngine<Schemes>
   ) {
     super("ChatContext");
-    this.addOutputPort({
-      key: "out",
-      name: "ResponseInput",
-      schema: ResponseInput,
-    });
+    this.addInputPort([
+      {
+        key: "exec",
+        name: "exec",
+        label: "add",
+        schema: Type.Literal("exec"),
+      },
+      {
+        key: "exec2",
+        name: "exec",
+        label: "add2",
+        schema: Type.Literal("exec"),
+      },
+      {
+        key: "systemPrompt",
+        name: "string",
+        label: "System Prompt",
+        schema: Type.String(),
+      },
+      {
+        key: "newMessage",
+        name: "ResponseInputMessageItem",
+        label: "New Message",
+        schema: ChatMessageItem,
+      },
+    ]);
+    this.addOutputPort([
+      {
+        key: "exec",
+        name: "exec",
+        label: "next",
+        schema: Type.Literal("exec"),
+      },
+
+      {
+        key: "out",
+        name: "ResponseInput",
+        schema: ResponseInput,
+      },
+    ]);
     this.addControl(
       "chatContext",
       new ResponseInputMessageControl({
@@ -54,5 +95,21 @@ export class ChatContextNode extends BaseNode<
     return { out: chatMessagesToResponseInput(messages) };
   }
 
-  async execute(): Promise<void> {}
+  async execute(
+    input: "exec" | "exec2",
+    forward: (output: "exec") => void
+  ): Promise<void> {
+    const { systemPrompt, newMessage } = (await this.dataflow.fetchInputs(
+      this.id
+    )) as {
+      systemPrompt?: string[];
+      newMessage?: ChatMessageItem[];
+    };
+    this.controls.chatContext.setSystemPrompt(systemPrompt?.[0] || "");
+    if (newMessage?.length) {
+      this.controls.chatContext.addMessage(newMessage[0]);
+    }
+    resetCacheDataflow(this.dataflow, this.id);
+    forward("exec");
+  }
 }
