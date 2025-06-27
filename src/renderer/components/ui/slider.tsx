821e0d267851c5b962a951d4ea13cb430c38f4fa
ahehead <nojoycrystal@gmail.com>
2025-06-27 16:40:44 +0900

    fix: update Slider component to use index as key for thumbs for improved rendering

--- src/renderer/components/ui/slider.tsx
+++ src/renderer/components/ui/slider.tsx
@@ -1,8 +1,7 @@
-
 import * as React from "react"
 import * as SliderPrimitive from "@radix-ui/react-slider"
-import { cn } from "renderer/lib/utils"
 
+import { cn } from "renderer/lib/utils"
 
 function Slider({
   className,
@@ -51,7 +50,7 @@ function Slider({
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
-          key={_values[index] ?? index}
+          key={index}
           className="border-primary bg-background ring-ring/50 block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
         />
       ))}
