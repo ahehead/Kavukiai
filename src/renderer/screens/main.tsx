e40a831324312c2c0568c85c27e9c100671ea0c8
ahehead <nojoycrystal@gmail.com>
2025-07-18 20:10:18 +0900

    refactor: インポートの順序を整理し、コードの可読性を向上

--- src/renderer/screens/main.tsx
+++ src/renderer/screens/main.tsx
@@ -1,16 +1,20 @@
-import SettingsModal from 'renderer/components/SettingsModal';
-import TabBar from 'renderer/features/tab/TabBar';
-import useNodeEditorSetup from 'renderer/hooks/useNodeEditorSetup';
-import useMainStore from 'renderer/hooks/MainStore';
-import { Toaster } from 'sonner';
+import { useCallback, useEffect, useState } from 'react'
+import SettingsModal from 'renderer/components/SettingsModal'
+import { MenuButton } from 'renderer/components/UIButton'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from 'renderer/components/ui/dropdown-menu'
+import { electronApiService } from 'renderer/features/services/appService'
+import TabBar from 'renderer/features/tab/TabBar'
+import useMainStore from 'renderer/hooks/MainStore'
+import { useFileOperations } from 'renderer/hooks/useFileOperations'
+import useNodeEditorSetup from 'renderer/hooks/useNodeEditorSetup'
+import { Toaster } from 'sonner'
 import { useShallow } from 'zustand/react/shallow'
-import { useCallback, useEffect, useState } from 'react';
-import { electronApiService } from 'renderer/features/services/appService';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from 'renderer/components/ui/dropdown-menu';
-import { useFileOperations } from 'renderer/hooks/useFileOperations';
-import { MenuButton } from 'renderer/components/UIButton';
-
-
 
 export function MainScreen() {
   const {
@@ -23,26 +27,28 @@ export function MainScreen() {
     setGraphAndHistory,
     getGraphAndHistory,
     updateFile,
-    clearHistory
-  } = useMainStore(useShallow(state => ({
-    files: state.files,
-    activeFileId: state.activeFileId,
-    getFileById: state.getFileById,
-    addFile: state.addFile,
-    removeFile: state.removeFile,
-    setActiveFileId: state.setActiveFileId,
-    setGraphAndHistory: state.setGraphAndHistory,
-    getGraphAndHistory: state.getGraphAndHistory,
-    updateFile: state.updateFile,
-    clearHistory: state.clearHistory
-  })));
+    clearHistory,
+  } = useMainStore(
+    useShallow(state => ({
+      files: state.files,
+      activeFileId: state.activeFileId,
+      getFileById: state.getFileById,
+      addFile: state.addFile,
+      removeFile: state.removeFile,
+      setActiveFileId: state.setActiveFileId,
+      setGraphAndHistory: state.setGraphAndHistory,
+      getGraphAndHistory: state.getGraphAndHistory,
+      updateFile: state.updateFile,
+      clearHistory: state.clearHistory,
+    }))
+  )
 
   // Reteエディタのセットアップ
   const { ref, setCurrentFileState, clearEditorHistory } = useNodeEditorSetup(
     activeFileId,
     getGraphAndHistory,
     setGraphAndHistory
-  );
+  )
 
   const { saveFile, closeFile, loadFile, newFile } = useFileOperations(
     files,
@@ -54,66 +60,81 @@ export function MainScreen() {
     addFile,
     removeFile,
     updateFile,
-    clearHistory,
-  );
+    clearHistory
+  )
 
   // 設定画面を表示するか
-  const [showSettings, setShowSettings] = useState(false);
+  const [showSettings, setShowSettings] = useState(false)
 
   // mainからのファイル読み込み通知を受け取り、ファイルを開く
   useEffect(() => {
-    const unsub = electronApiService.onFileLoadedRequest(async (e, fileData) => await loadFile(fileData));
-    return () => { unsub() };
-  }, [loadFile]);
+    const unsub = electronApiService.onFileLoadedRequest(
+      async (_e, fileData) => await loadFile(fileData)
+    )
+    return () => {
+      unsub()
+    }
+  }, [loadFile])
 
   useEffect(() => {
     // 設定画面オープン指示
-    const unsubOpen = electronApiService.onOpenSettings(() => setShowSettings(true));
-    return () => { unsubOpen(); };
-  }, [setShowSettings]);
+    const unsubOpen = electronApiService.onOpenSettings(() =>
+      setShowSettings(true)
+    )
+    return () => {
+      unsubOpen()
+    }
+  }, [setShowSettings])
 
   useEffect(() => {
     // mainからの保存指示を受け取り、現在開いているファイルを保存
-    const unsubSave = electronApiService.onSaveGraphInitiate(async () => await saveFile(activeFileId));
-    return () => { unsubSave() };
-  }, [activeFileId, saveFile]);
+    const unsubSave = electronApiService.onSaveGraphInitiate(
+      async () => await saveFile(activeFileId)
+    )
+    return () => {
+      unsubSave()
+    }
+  }, [activeFileId, saveFile])
 
   const handleNewFile = async () => {
-    newFile();
-  };
+    newFile()
+  }
 
   // タブ選択
   const handleSelect = (id: string) => {
     // 選択前のファイルの状態を保存
-    setCurrentFileState();
-    setActiveFileId(id);
-  };
-
+    setCurrentFileState()
+    setActiveFileId(id)
+  }
 
   // ファイルを閉じる
   const handleCloseFile = async (id: string, e: React.MouseEvent) => {
-    e.stopPropagation();
-    closeFile(id);
-  };
+    e.stopPropagation()
+    closeFile(id)
+  }
 
   // ファイルを開くボタン
   const handleLoadFile = useCallback(async () => {
-    const result = await electronApiService.loadFile();
-    if (!result) return;
-    await loadFile(result);
-  }, [loadFile]);
+    const result = await electronApiService.loadFile()
+    if (!result) return
+    await loadFile(result)
+  }, [loadFile])
 
   return (
     <div className="flex flex-col fixed inset-0">
       {/* タイトルバー */}
-      <div className="flex titlebar bg-titlebar" >
+      <div className="flex titlebar bg-titlebar">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <MenuButton>File</MenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuGroup>
-              <DropdownMenuItem onClick={async () => await saveFile(activeFileId)}>Save</DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={async () => await saveFile(activeFileId)}
+              >
+                Save
+              </DropdownMenuItem>
               <DropdownMenuItem onClick={handleLoadFile}>Open</DropdownMenuItem>
             </DropdownMenuGroup>
           </DropdownMenuContent>
@@ -124,29 +145,20 @@ export function MainScreen() {
       <main className="flex flex-1 flex-col">
         {files.length === 0 ? (
           <div className="flex-1 flex items-center justify-center bg-blue-100 border-8 rounded-lg border-white">
-            <div className='flex flex-col items-start dialog-animate-up'>
-              <ul className=''>
+            <div className="flex flex-col items-start dialog-animate-up">
+              <ul className="">
                 <li className="mb-2  rounded hover:bg-gray-100 bg-background">
-                  <button
-                    className="px-4 py-2"
-                    onClick={handleNewFile}
-                  >
+                  <button className="px-4 py-2" onClick={handleNewFile}>
                     ・ 新規作成
                   </button>
                 </li>
-                <li className='mb-2 bg-background rounded hover:bg-gray-100'>
-                  <button
-                    className="px-4 py-2"
-                    onClick={handleNewFile}
-                  >
+                <li className="mb-2 bg-background rounded hover:bg-gray-100">
+                  <button className="px-4 py-2" onClick={handleNewFile}>
                     ・ テンプレートから作成
                   </button>
                 </li>
-                <li className='mb-2 bg-background rounded hover:bg-gray-100'>
-                  <button
-                    className="px-4 py-2"
-                    onClick={handleLoadFile}
-                  >
+                <li className="mb-2 bg-background rounded hover:bg-gray-100">
+                  <button className="px-4 py-2" onClick={handleLoadFile}>
                     ・ ファイルを開く
                   </button>
                 </li>
@@ -176,7 +188,7 @@ export function MainScreen() {
           )
         }
         <Toaster richColors={true} expand={true} offset={5} />
-      </main >
+      </main>
     </div>
   )
 }
