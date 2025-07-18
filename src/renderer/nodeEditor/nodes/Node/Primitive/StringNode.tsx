c129cbeca92d578dff69b8d14c7ea498fca8b482
ahehead <nojoycrystal@gmail.com>
2025-07-18 19:58:24 +0900

    refactor: インポートの順序を整理し、コードの可読性を向上

--- src/renderer/nodeEditor/nodes/Node/Primitive/StringNode.tsx
+++ src/renderer/nodeEditor/nodes/Node/Primitive/StringNode.tsx
@@ -1,33 +1,31 @@
-
-import { BaseNode } from "renderer/nodeEditor/types/Node/BaseNode";
-import type { HistoryPlugin } from 'rete-history-plugin';
-import type { AreaPlugin } from 'rete-area-plugin';
-import type { DataflowEngine } from 'rete-engine';
-import type { AreaExtra, TypedSocket, Schemes } from 'renderer/nodeEditor/types';
-
-import { resetCacheDataflow } from '../../util/resetCacheDataflow';
-import type { SerializableDataNode } from "renderer/nodeEditor/types/Node/SerializableDataNode";
-import { InputValueControl } from "renderer/nodeEditor/nodes/Controls/input/InputValue";
+import { InputValueControl } from 'renderer/nodeEditor/nodes/Controls/input/InputValue'
+import type { AreaExtra, Schemes, TypedSocket } from 'renderer/nodeEditor/types'
+import { BaseNode } from 'renderer/nodeEditor/types/Node/BaseNode'
+import type { SerializableDataNode } from 'renderer/nodeEditor/types/Node/SerializableDataNode'
+import type { AreaPlugin } from 'rete-area-plugin'
+import type { DataflowEngine } from 'rete-engine'
+import type { HistoryPlugin } from 'rete-history-plugin'
+import { resetCacheDataflow } from '../../util/resetCacheDataflow'
 // 短い文字列入力ノード
-export class StringNode extends BaseNode<
-  object,
-  { out: TypedSocket },
-  { textInput: InputValueControl<string> }
-> implements SerializableDataNode {
+export class StringNode
+  extends BaseNode<
+    object,
+    { out: TypedSocket },
+    { textInput: InputValueControl<string> }
+  >
+  implements SerializableDataNode {
   constructor(
     initial: string,
     history: HistoryPlugin<Schemes>,
     area: AreaPlugin<Schemes, AreaExtra>,
     dataflow: DataflowEngine<Schemes>
   ) {
-    super('String');
+    super('String')
 
-    this.addOutputPort(
-      {
-        key: 'out',
-        typeName: "string",
-      }
-    );
+    this.addOutputPort({
+      key: 'out',
+      typeName: 'string',
+    })
 
     this.addControl(
       'textInput',
@@ -37,26 +35,26 @@ export class StringNode extends BaseNode<
         editable: true,
         history: history,
         area: area,
-        onChange: (v: string) => {
-          resetCacheDataflow(dataflow, this.id);
-        }
+        onChange: (_v: string) => {
+          resetCacheDataflow(dataflow, this.id)
+        },
       })
-    );
+    )
   }
 
   data(): { out: string } {
-    return { out: this.controls.textInput.value || '' };
+    return { out: this.controls.textInput.value || '' }
   }
 
   async execute(): Promise<void> { }
 
   serializeControlValue(): { data: { value: string } } {
     return {
-      data: { value: this.controls.textInput.value || "" }
-    };
+      data: { value: this.controls.textInput.value || '' },
+    }
   }
 
   deserializeControlValue(data: { value: string }): void {
-    this.controls.textInput.setValue(data.value);
+    this.controls.textInput.setValue(data.value)
   }
 }
