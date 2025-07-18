0c30a02a1adba4dccfbda1e51f67d0e6716e8565
ahehead <nojoycrystal@gmail.com>
2025-07-18 19:55:11 +0900

    refactor: インポートの整理とコードの整形を実施

--- src/renderer/nodeEditor/nodes/components/CustomBaseNode.tsx
+++ src/renderer/nodeEditor/nodes/components/CustomBaseNode.tsx
@@ -1,13 +1,23 @@
+import { useRef } from 'react'
+import { ControlLabel } from 'renderer/nodeEditor/nodes/components/common/NodeControlParts'
+import {
+  NodeBody,
+  NodeContainer,
+  NodeControlsWrapper,
+  NodeHeader,
+  NodeTitle,
+} from 'renderer/nodeEditor/nodes/components/common/NodeParts'
+import {
+  NodeInputPort,
+  NodeOutputPort,
+  NodeSocketLabel,
+  NodeSocketsWrapper,
+  NodeSocketTypeLabel,
+} from 'renderer/nodeEditor/nodes/components/common/NodeSocketParts'
+import type { AreaPlugin } from 'rete-area-plugin'
+import type { HistoryPlugin } from 'rete-history-plugin'
 import { Presets, type RenderEmit } from 'rete-react-plugin'
 import type { AreaExtra, NodeInterface, Schemes } from '../../types/Schemes'
-import { NodeContainer, NodeHeader, NodeTitle, NodeControlsWrapper, NodeBody, } from 'renderer/nodeEditor/nodes/components/common/NodeParts'
-import { NodeOutputPort, NodeSocketLabel, NodeSocketTypeLabel } from "renderer/nodeEditor/nodes/components/common/NodeSocketParts"
-import { NodeInputPort } from "renderer/nodeEditor/nodes/components/common/NodeSocketParts"
-import { NodeSocketsWrapper } from "renderer/nodeEditor/nodes/components/common/NodeSocketParts"
-import type { AreaPlugin } from 'rete-area-plugin'
-import { useRef } from 'react'
-import type { HistoryPlugin } from 'rete-history-plugin'
-import { ControlLabel } from 'renderer/nodeEditor/nodes/components/common/NodeControlParts'
 import { withTooltip } from './common/TooltipSetting'
 import { useNodeResize } from './hooks/useNodeResize'
 
@@ -21,22 +31,27 @@ export function createCustomNode(
   history: HistoryPlugin<Schemes>,
   getZoom: () => number
 ) {
-  return function CustomNode<Scheme extends Schemes>({ data, emit }: Props<Scheme>) {
+  return function CustomNode<Scheme extends Schemes>({
+    data,
+    emit,
+  }: Props<Scheme>) {
     const panelRef = useRef<HTMLDivElement>(null)
-    const { startResize, nodeMinWidth, nodeMinHeight, clearNodeSize } = useNodeResize({
-      node: data,
-      area,
-      history,
-      getZoom,
-      panelRef
-    });
+    const { startResize, nodeMinWidth, clearNodeSize } =
+      useNodeResize({
+        node: data,
+        area,
+        history,
+        getZoom,
+        panelRef,
+      })
     const inputs = Object.entries(data.inputs)
     const outputs = Object.entries(data.outputs)
     const controls = Object.entries(data.controls)
     const { id, label, width, height, selected } = data
 
-
-    function sortByIndex<T extends [string, undefined | { index?: number }][]>(entries: T) {
+    function sortByIndex<T extends [string, undefined | { index?: number }][]>(
+      entries: T
+    ) {
       entries.sort((a, b) => (a[1]?.index || 0) - (b[1]?.index || 0))
     }
     sortByIndex(inputs)
@@ -44,9 +59,9 @@ export function createCustomNode(
     sortByIndex(controls)
 
     const handleDoubleClickResize = async (e: React.MouseEvent) => {
-      e.stopPropagation();
-      await clearNodeSize();
-    };
+      e.stopPropagation()
+      await clearNodeSize()
+    }
 
     return (
       <NodeContainer
@@ -56,7 +71,7 @@ export function createCustomNode(
         nodeType={label}
         style={{
           width: `${Number.isFinite(width) ? width : nodeMinWidth}px`,
-          height: Number.isFinite(height) ? `${height}px` : 'auto'
+          height: Number.isFinite(height) ? `${height}px` : 'auto',
         }}
       >
         <NodeHeader status={data.status} nodeType={label}>
@@ -66,42 +81,44 @@ export function createCustomNode(
         <NodeBody>
           <NodeSocketsWrapper>
             {/* Outputs */}
-            {outputs.map(([key, output]) =>
-              output && (
-                <NodeOutputPort
-                  key={key}
-                  data-testid={`output-${key}`}
-                >
-                  {output.label &&
-                    <NodeSocketLabel isExec={output.socket.isExec} data-testid="output-title">
-                      {output.label}
-                    </NodeSocketLabel>}
+            {outputs.map(
+              ([key, output]) =>
+                output && (
+                  <NodeOutputPort key={key} data-testid={`output-${key}`}>
+                    {output.label && (
+                      <NodeSocketLabel
+                        isExec={output.socket.isExec}
+                        data-testid="output-title"
+                      >
+                        {output.label}
+                      </NodeSocketLabel>
+                    )}
 
-                  {!output.socket.isExec && (
-                    withTooltip(
-                      <NodeSocketTypeLabel data-testid="output-type">{output.socket.name}</NodeSocketTypeLabel>,
-                      true,
-                      output.socket.tooltip
-                    )
-                  )}
+                    {!output.socket.isExec &&
+                      withTooltip(
+                        <NodeSocketTypeLabel data-testid="output-type">
+                          {output.socket.name}
+                        </NodeSocketTypeLabel>,
+                        true,
+                        output.socket.tooltip
+                      )}
 
-                  <Presets.classic.RefSocket
-                    name="output-socket"
-                    side="output"
-                    socketKey={key}
-                    nodeId={id}
-                    emit={emit}
-                    payload={output.socket}
-                    data-testid="output-socket"
-                  />
-
-                </NodeOutputPort>
-              )
+                    <Presets.classic.RefSocket
+                      name="output-socket"
+                      side="output"
+                      socketKey={key}
+                      nodeId={id}
+                      emit={emit}
+                      payload={output.socket}
+                      data-testid="output-socket"
+                    />
+                  </NodeOutputPort>
+                )
             )}
 
             {/* Inputs */}
             {inputs.map(([key, input]) => {
-              if (!input) return null;
+              if (!input) return null
 
               return (
                 // NodeのInputは二種類、ソケットとコントロールモードがある。
@@ -125,14 +142,15 @@ export function createCustomNode(
                         data-testid="input-socket"
                       />
 
-                      {!input.socket.isExec && (
+                      {!input.socket.isExec &&
                         withTooltip(
-                          <NodeSocketTypeLabel data-testid="input-type">{input.socket.name}</NodeSocketTypeLabel>,
+                          <NodeSocketTypeLabel data-testid="input-type">
+                            {input.socket.name}
+                          </NodeSocketTypeLabel>,
                           true,
                           input.socket.tooltip
-                        )
-                      )}
-                      {input.label && (
+                        )}
+                      {input.label &&
                         withTooltip(
                           <NodeSocketLabel
                             isExec={input.socket.isExec}
@@ -143,26 +161,27 @@ export function createCustomNode(
                           </NodeSocketLabel>,
                           false,
                           input.tooltip
-                        )
-                      )}
+                        )}
                     </>
                   )}
                   {input.control && input.showControl && (
                     <>
-                      {input.control.opts.label && input.control.opts.cols !== 0 && (
-                        <ControlLabel
-                          cols={input.control.opts.cols}
-                          htmlFor={input.control.id}
-                          isRequired={input.require}
-                        >
-                          {withTooltip(
-                            <div className='inline-block'>{input.control.opts.label}</div>,
-                            false,
-                            input.tooltip
-                          )}
-                        </ControlLabel>
-                      )
-                      }
+                      {input.control.opts.label &&
+                        input.control.opts.cols !== 0 && (
+                          <ControlLabel
+                            cols={input.control.opts.cols}
+                            htmlFor={input.control.id}
+                            isRequired={input.require}
+                          >
+                            {withTooltip(
+                              <div className="inline-block">
+                                {input.control.opts.label}
+                              </div>,
+                              false,
+                              input.tooltip
+                            )}
+                          </ControlLabel>
+                        )}
                       <Presets.classic.RefControl
                         key={key}
                         name="input-control"
@@ -174,11 +193,7 @@ export function createCustomNode(
                   )}
                 </NodeInputPort>
               )
-            }
-
-
-            )}
-
+            })}
           </NodeSocketsWrapper>
           {/* Controls */}
           <NodeControlsWrapper>
@@ -194,9 +209,8 @@ export function createCustomNode(
               ) : null
             )}
           </NodeControlsWrapper>
-
         </NodeBody>
-        <div
+        <button
           className="absolute right-0 bottom-0 w-4 h-4 cursor-se-resize bg-transparent"
           onPointerDown={startResize}
           onDoubleClick={handleDoubleClickResize}
