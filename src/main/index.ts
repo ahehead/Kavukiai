0a122d11e3e3f58c505373a6d6ff8fc43a059d8f
ahehead <nojoycrystal@gmail.com>
2025-07-19 13:42:38 +0900

    refactor: インポートの順序を整理

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,8 @@
 import { app } from "electron";
 import { makeAppWithSingleInstanceLock } from "lib/electron-app/factories/app/instance";
 import { makeAppSetup } from "lib/electron-app/factories/app/setup";
-import { MainWindow } from "./windows/main";
 import { registerIpcHandlers } from "./ipc";
+import { MainWindow } from "./windows/main";
 
 makeAppWithSingleInstanceLock(async () => {
   await app.whenReady();
