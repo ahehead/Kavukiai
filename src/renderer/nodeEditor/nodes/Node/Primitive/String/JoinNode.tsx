4e008c15caa018e35976ccc9668cb84af24f66bb
ahehead <nojoycrystal@gmail.com>
2025-07-20 02:41:14 +0900

    feat: JoinNodeをSerializableInputsNodeに変更し、separatorの制御を追加

--- src/renderer/nodeEditor/nodes/Node/Primitive/String/JoinNode.tsx
+++ src/renderer/nodeEditor/nodes/Node/Primitive/String/JoinNode.tsx
@@ -1,27 +1,49 @@
-import type { TypedSocket } from 'renderer/nodeEditor/types'
-import { BaseNode } from 'renderer/nodeEditor/types/Node/BaseNode'
+import { InputValueControl } from 'renderer/nodeEditor/nodes/Controls/input/InputValue'
+import { getInputValue } from 'renderer/nodeEditor/nodes/util/getInput';
+import { resetCacheDataflow } from 'renderer/nodeEditor/nodes/util/resetCacheDataflow';
+import { unescapeSeparator } from 'renderer/nodeEditor/nodes/util/unescapeSeparator';
+import type { Schemes, TypedSocket } from 'renderer/nodeEditor/types'
+import { SerializableInputsNode } from 'renderer/nodeEditor/types/Node/SerializableInputsNode';
+import type { DataflowEngine } from 'rete-engine';
 
 // JoinNode: 文字列配列を結合して返す
-export class JoinNode extends BaseNode<
+export class JoinNode extends SerializableInputsNode<
   'Join',
   { list: TypedSocket; separator: TypedSocket },
   { out: TypedSocket },
   object
 > {
-  constructor() {
+  constructor(
+    private dataflow: DataflowEngine<Schemes>
+  ) {
     super('Join')
     this.addInputPort([
       { key: 'list', typeName: 'StringArray', label: 'list' },
-      { key: 'separator', typeName: 'string', label: 'separator' },
+      {
+        key: 'separator',
+        typeName: 'string',
+        label: 'separator',
+        control: new InputValueControl<string>({
+          value: ',',
+          type: 'string',
+          editable: true,
+          label: 'separator',
+          onChange: (_value) => {
+            resetCacheDataflow(this.dataflow, this.id)
+          }
+        },
+        )
+      }
     ])
     this.addOutputPort({ key: 'out', typeName: 'string', label: 'out' })
   }
 
   data(inputs: { list?: string[][]; separator?: string[] }): { out: string } {
     const arr = inputs.list?.[0] ?? []
-    const sep = inputs.separator?.[0] ?? ''
+    const rawSep = getInputValue(this.inputs, "separator", inputs);
+    const sep = unescapeSeparator(rawSep);
     return { out: Array.isArray(arr) ? arr.join(sep) : '' }
   }
 
-  async execute(): Promise<void> {}
+  async execute(): Promise<void> { }
 }
