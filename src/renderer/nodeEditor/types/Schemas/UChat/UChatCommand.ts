258270777f252c5408041533ec9abc1d671957d3
ahehead <nojoycrystal@gmail.com>
2025-08-02 23:32:17 +0900

    feat: UChatCommandEvent型を新規作成し、OpenAIのレスポンスを変換するためのイベント型を定義

---
+++ src/renderer/nodeEditor/types/Schemas/UChat/UChatCommand.ts
@@ -0,0 +1,40 @@
+import type { Static } from "@sinclair/typebox";
+import { Type } from "@sinclair/typebox";
+import { UChatMessage } from "./UChatMessage";
+
+// OpenAIのレスポンスをChatCommandEventに変換するためのイベント型
+export const UChatCommandEvent = Type.Union([
+  Type.Object({
+    type: Type.Literal("start"),
+    message: UChatMessage,
+  }),
+  Type.Object({
+    type: Type.Literal("setInfo"),
+    message: UChatMessage,
+  }),
+  Type.Object({
+    type: Type.Literal("delta"),
+    delta: Type.String(),
+  }),
+  Type.Object({
+    type: Type.Literal("done"),
+    text: Type.String(),
+  }),
+  Type.Object({
+    type: Type.Literal("error"),
+    message: Type.Optional(Type.String()),
+  }),
+  Type.Object({
+    type: Type.Literal("response"),
+    messages: Type.Array(UChatMessage),
+  }),
+]);
+
+export type UChatCommandEvent = Static<typeof UChatCommandEvent>;
+
+export const UChatCommandEventOrNull = Type.Union([
+  UChatCommandEvent,
+  Type.Null(),
+]);
+
+export type UChatCommandEventOrNull = Static<typeof UChatCommandEventOrNull>;
