1d9fdc9fe3aa50428d69c23ce981f1ba957687e0
ahehead <nojoycrystal@gmail.com>
2025-08-09 23:48:20 +0900

    refactor: ContextMenuPluginのインポートを整理し、createReteContextMenuItemsをcreateNodeFactoryMenuItemsに置き換え

--- src/renderer/nodeEditor/features/contextMenu/setup/SetupContextMenu.tsx
+++ src/renderer/nodeEditor/features/contextMenu/setup/SetupContextMenu.tsx
@@ -1,6 +1,5 @@
 import type { NodeEditor } from 'rete'
 import type { AreaPlugin } from 'rete-area-plugin'
-import { ContextMenuPlugin } from 'rete-context-menu-plugin'
 import type { Item } from 'rete-context-menu-plugin/_types/types'
 import type { ControlFlowEngine } from 'rete-engine'
 import type { HistoryActions, HistoryPlugin } from 'rete-history-plugin'
@@ -8,15 +7,23 @@ import {
   contextMenuStructure,
   type NodeDeps,
 } from '../../../nodes/nodeFactories'
-import type { AreaExtra, NodeInterface, Schemes } from '../../../types/Schemes'
+import type {
+  AreaExtra,
+  NodeInterface,
+  NodeTypes,
+  Schemes,
+} from '../../../types/Schemes'
 import type { DataflowEngine } from '../../safe-dataflow/dataflowEngin'
-import { createReteContextMenuItems } from './items/createContextMenu'
+import { ContextMenuPlugin } from '..'
+import { createCopyItem } from './items/copy'
+import { createNodeFactoryMenuItems } from './items/createContextMenu'
 import { createDeleteConnectionItem } from './items/createDeleteConnectionItem'
 import { createDeleteNodeItem } from './items/createDeleteNodeItem'
 import {
   createToggleInputControlMenuItem,
   filterInputControls,
 } from './items/createToggleInputControlMenuItem'
+import { createPasteItem } from './items/paste'
 
 type ContextMenuDependencies = {
   editor: NodeEditor<Schemes>
@@ -47,36 +54,30 @@ export function setupContextMenu({
 
   return new ContextMenuPlugin<Schemes>({
     items: context => {
-      // 右クリックを押されたときの場所。
+      // 右クリックを押されたときの座標。
       const pointer = area.area.pointer
+
+      // 何もない場所で右クリックされた場合
       if (context === 'root') {
         return {
           searchBar: true,
-          list: createReteContextMenuItems(
+          list: [...createNodeFactoryMenuItems(
             contextMenuStructure,
             editor,
             nodeDeps,
             pointer
           ),
+          createPasteItem(nodeDeps)]
         }
       }
 
       const listItems: Item[] = []
 
       if (isNode(context)) {
-        // node のinputにcontrolがある場合、showControlをtoggleするメニューを追加
-        const inputlist = filterInputControls(context.inputs)
-        if (inputlist.length > 0) {
-          listItems.push(
-            createToggleInputControlMenuItem(
-              context,
-              editor,
-              area,
-              dataflow,
-              inputlist
-            )
-          )
-        }
+        // node のinputにcontrolがある場合、(showControlをtoggleする)メニュー項目を追加
+        addToggleInputMenuItem(context, listItems, editor, area, dataflow)
+        // コピー機能
+        listItems.push(createCopyItem(context, editor, area))
       }
 
       if (isConnection(context)) {
@@ -91,4 +92,33 @@ export function setupContextMenu({
       }
     },
   })
+}
+
+/**
+ * node のinputにcontrolがある場合、(showControlをtoggleする)メニュー項目を追加
+ * @param context
+ * @param listItems
+ * @param editor
+ * @param area
+ * @param dataflow
+ */
+function addToggleInputMenuItem(
+  context: NodeTypes,
+  listItems: Item[],
+  editor: NodeEditor<Schemes>,
+  area: AreaPlugin<Schemes, AreaExtra>,
+  dataflow: DataflowEngine<Schemes>
+) {
+  const inputlist = filterInputControls(context.inputs)
+  if (inputlist.length > 0) {
+    listItems.push(
+      createToggleInputControlMenuItem(
+        context,
+        editor,
+        area,
+        dataflow,
+        inputlist
+      )
+    )
+  }
 }
