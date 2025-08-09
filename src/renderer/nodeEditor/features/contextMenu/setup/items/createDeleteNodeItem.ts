9f9313574bf70b963e2fbfc5f6b7bb4398b1056c
ahehead <nojoycrystal@gmail.com>
2025-08-10 07:34:51 +0900

    refactor: 不要なコードを削除し、ノード削除処理を簡素化

--- src/renderer/nodeEditor/features/contextMenu/setup/items/createDeleteNodeItem.ts
+++ src/renderer/nodeEditor/features/contextMenu/setup/items/createDeleteNodeItem.ts
@@ -1,7 +1,7 @@
 import type { NodeEditor } from "rete";
-import type { Schemes } from "../../../../types/Schemes";
-import { removeNodeWithConnections } from "../../../../nodes/util/removeNode";
 import type { Item } from "rete-context-menu-plugin/_types/types";
+import { removeNodeWithConnections } from "../../../../nodes/util/removeNode";
+import type { Schemes } from "../../../../types/Schemes";
 
 export function createDeleteNodeItem(
   context: { id: string },
@@ -12,12 +12,8 @@ export function createDeleteNodeItem(
     key: "delete-node",
     async handler() {
       await removeNodeWithConnections(editor, context.id);
-      const nodes = editor.getNodes().filter((node) => node.selected);
-      // console.log("Selected nodes to delete:", nodes);
-      if (nodes.length > 0) {
-        await Promise.all(
-          nodes.map((node) => removeNodeWithConnections(editor, node.id))
-        );
+      for (const node of editor.getNodes().filter((node) => node.selected)) {
+        await removeNodeWithConnections(editor, node.id);
       }
     },
   };
