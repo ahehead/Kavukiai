0c0b90d8770154d520c52cda8d3bc9f2657710e2
ahehead <nojoycrystal@gmail.com>
2025-08-21 19:18:50 +0900

    refactor: InputValueControlとInputValueControlViewのコードを整理し、可読性を向上

--- src/renderer/nodeEditor/nodes/Controls/input/InputValue.tsx
+++ src/renderer/nodeEditor/nodes/Controls/input/InputValue.tsx
@@ -1,91 +1,93 @@
-import type { JSX } from "react";
-import { Drag } from "rete-react-plugin";
-import { inputValueStyles } from "renderer/nodeEditor/nodes/components/common/NodeControlParts";
-import type { ControlJson } from "shared/JsonType";
-import { BaseControl, useControlValue, type ControlOptions } from "renderer/nodeEditor/types";
+import type { JSX } from 'react'
+import { inputValueStyles } from 'renderer/nodeEditor/nodes/components/common/NodeControlParts'
+import {
+  BaseControl,
+  type ControlOptions,
+  useControlValue,
+} from 'renderer/nodeEditor/types'
+import { Drag } from 'rete-react-plugin'
+import type { ControlJson } from 'shared/JsonType'
 
 export interface InputValueActionParams<T> extends ControlOptions<T> {
-  value: T;
-  type?: "string" | "number";
-  step?: number;
+  value: T
+  type?: 'string' | 'number'
+  step?: number
 }
 
 // stringまたはnumber入力用コントロール
-export class InputValueControl<T extends string | number> extends BaseControl<T, InputValueActionParams<T>> {
-  value: T;
-  type: "string" | "number";
-  step?: number;
+export class InputValueControl<T extends string | number> extends BaseControl<
+  T,
+  InputValueActionParams<T>
+> {
+  value: T
+  type: 'string' | 'number'
+  step?: number
 
   constructor(options: InputValueActionParams<T>) {
-    super(options);
-    this.value = options.value;
-    this.type = options?.type ?? (typeof options.value === "string" ? "string" : "number");
-    this.step = options?.step;
+    super(options)
+    this.value = options.value
+    this.type =
+      options?.type ?? (typeof options.value === 'string' ? 'string' : 'number')
+    this.step = options?.step
   }
 
   setValue(value: T) {
-    this.value = value;
-    this.opts.onChange?.(value);
-    this.notify();
+    this.value = value
+    this.opts.onChange?.(value)
+    this.notify()
   }
 
   getValue(): T {
-    return this.value;
+    return this.value
   }
 
   override toJSON(): ControlJson {
     return {
       data: {
         value: this.value,
-        type: this.type,
-        label: this.opts.label,
-        editable: this.opts.editable,
-        step: this.step,
       },
-    };
+    }
   }
   override setFromJSON({ data }: ControlJson): void {
-    const { value, type, label, editable, step } = data as any;
-    this.value = value;
-    this.type = type;
-    this.opts.label = label;
-    this.opts.editable = editable;
-    this.step = step;
-    this.notify();
+    const { value } = data as any
+    this.value = value
+    this.notify()
   }
 }
 
 // カスタムコンポーネント
-export function InputValueControlView<T extends string | number>(props: {
-  data: InputValueControl<T>;
+export function InputValueControlView<T extends string | number>({
+  data,
+}: {
+  data: InputValueControl<T>
 }): JSX.Element {
-  const control = props.data;
-  const value = useControlValue(control);
+  const value = useControlValue(data)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const rawValue = e.target.value;
-    const newValue = (control.type === "number" ? Number.parseFloat(rawValue) : rawValue) as T;
+    const rawValue = e.target.value
+    const newValue = (
+      data.type === 'number' ? Number.parseFloat(rawValue) : rawValue
+    ) as T
 
-    control.addHistory(value, newValue)
-    control.setValue(newValue);
-  };
+    data.addHistory(value, newValue)
+    data.setValue(newValue)
+  }
 
   return (
     <Drag.NoDrag>
       <div className="w-full">
         <input
-          id={control.id}
-          type={control.type === "number" ? "number" : "text"}
-          step={control.type === "number" ? control.step : undefined}
+          id={data.id}
+          type={data.type === 'number' ? 'number' : 'text'}
+          step={data.type === 'number' ? data.step : undefined}
           value={value}
-          readOnly={!control.opts.editable}
-          onFocus={() => control.addHistory(value, value)}
-          onChange={control.opts.editable ? handleChange : undefined}
-          className={inputValueStyles({ editable: control.opts.editable })}
+          readOnly={!data.opts.editable}
+          onFocus={() => data.addHistory(value, value)}
+          onChange={data.opts.editable ? handleChange : undefined}
+          className={inputValueStyles({ editable: data.opts.editable })}
           placeholder=""
         />
       </div>
     </Drag.NoDrag>
-  );
+  )
 }
-
