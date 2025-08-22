009903dba7543d23da1f895a7dab410ace6ebbf2
ahehead <nojoycrystal@gmail.com>
2025-08-23 02:16:20 +0900

    feat: Monacoエディタのテーマを定義するロジックを追加

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -35,7 +35,24 @@ self.MonacoEnvironment = {
 }
 
 loader.config({ monaco })
-await loader.init();
+const mona = await loader.init();
+
+mona.editor.defineTheme('logViewerLight', {
+  base: 'vs',
+  inherit: true,
+  rules: [],
+  colors: {
+    'editor.background': '#f3f4f6',
+    'editorGutter.background': '#f3f4f6',
+    'editor.selectionBackground': '#2563eb66',
+    'editor.inactiveSelectionBackground': '#93c5fd66',
+    'editor.selectionHighlightBackground': '#2563eb33',
+    'editor.selectionForeground': '#111827',
+    // フォーカス時の青い枠線を除去
+    focusBorder: '#00000000',
+    'editor.focusedBorder': '#00000000',
+  },
+})
 
 ReactDom.createRoot(document.querySelector('app') as HTMLElement).render(
   <React.StrictMode>
