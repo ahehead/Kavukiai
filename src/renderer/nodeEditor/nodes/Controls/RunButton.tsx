9c877e14d32ce20b81993e2d1066ef4f6d38e5f9
ahehead <nojoycrystal@gmail.com>
2025-08-23 02:07:42 +0900

    fix: RunButtonコンポーネントのスタイルを修正

--- src/renderer/nodeEditor/nodes/Controls/RunButton.tsx
+++ src/renderer/nodeEditor/nodes/Controls/RunButton.tsx
@@ -27,10 +27,10 @@ function RunButton(props: {
   return (
     <Drag.NoDrag>
       <button
-        className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-bold text-node-fg rounded-lg group bg-gradient-to-br from-node-execSocket to-dataSocket w-full drop-shadow-md active:drop-shadow-none -translate-y-1 active:translate-y-0 transform-gpu transition-all duration-100 ease-out"
+        className="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-bold text-node-fg rounded-lg group bg-gradient-to-br from-node-execSocket to-dataSocket w-full drop-shadow-md  active:drop-shadow-none -translate-y-1 active:translate-y-0 transform-gpu transition-all duration-100 ease-out"
         onClick={props.onClick}
       >
-        <span className="relative px-5 py-2.5 bg-background rounded-md w-full group-active:bg-node-execSocket-light">
+        <span className="relative px-5 py-2.5 bg-background rounded-md w-full hover:bg-node-execSocket-lightgroup-active:bg-node-execSocket-light">
           {props.label}
         </span>
       </button>
