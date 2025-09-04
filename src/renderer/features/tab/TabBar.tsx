16ac1241fe35f60067f227f298c2d96fe879a003
ahehead <nojoycrystal@gmail.com>
2025-09-04 21:05:30 +0900

    feat: タブコンポーネントを移動

--- src/renderer/features/tab/TabBar.tsx
+++ src/renderer/features/tab/TabBar.tsx
@@ -1,23 +1,27 @@
-import { memo } from 'react';
-import type { File } from 'shared/AppType';
-import { useIsFileDirty } from '../dirty-check/useIsFileDirty';
-import { CloseFileButton, PlusButton } from '../../components/UIButton';
-import { TabAddButtonSpace, TabBarComponent, TabItems, TabItemComponent, TabTitle } from 'renderer/components/TabComponent';
-
-
+import { memo } from 'react'
+import {
+  TabAddButtonSpace,
+  TabBarComponent,
+  TabItemComponent,
+  TabItems,
+  TabTitle,
+} from 'renderer/features/tab/TabComponent'
+import type { File } from 'shared/AppType'
+import { CloseFileButton, PlusButton } from '../../components/UIButton'
+import { useIsFileDirty } from '../dirty-check/useIsFileDirty'
 
 export default function TabBar({
   files,
   activeFileId,
   onSelect,
   onClose,
-  onNewFile
+  onNewFile,
 }: {
-  files: File[];
-  activeFileId: string | null;
-  onSelect: (id: string) => void;
-  onClose: (id: string, e: React.MouseEvent) => void;
-  onNewFile: () => void;
+  files: File[]
+  activeFileId: string | null
+  onSelect: (id: string) => void
+  onClose: (id: string, e: React.MouseEvent) => void
+  onNewFile: () => void
 }) {
   return (
     <TabBarComponent>
@@ -36,31 +40,28 @@ export default function TabBar({
         <PlusButton onClick={onNewFile} />
       </TabAddButtonSpace>
     </TabBarComponent>
-  );
-}
-
-
-const TabItem = memo(({
-  file,
-  active,
-  onSelect,
-  onClose
-}: {
-  file: { id: string; title: string };
-  active: boolean;
-  onSelect: (id: string) => void;
-  onClose: (id: string, e: React.MouseEvent) => void;
-}) => {
-  const isDirty = useIsFileDirty(file.id);
-
-  return (
-    <TabItemComponent
-      active={active}
-      onClick={() => onSelect(file.id)}
-    >
-      <TabTitle>{file.title}</TabTitle>
-      <CloseFileButton isDirty={isDirty} onClick={e => onClose(file.id, e)} />
-    </TabItemComponent>
   )
 }
+
+const TabItem = memo(
+  ({
+    file,
+    active,
+    onSelect,
+    onClose,
+  }: {
+    file: { id: string; title: string }
+    active: boolean
+    onSelect: (id: string) => void
+    onClose: (id: string, e: React.MouseEvent) => void
+  }) => {
+    const isDirty = useIsFileDirty(file.id)
+
+    return (
+      <TabItemComponent active={active} onClick={() => onSelect(file.id)}>
+        <TabTitle>{file.title}</TabTitle>
+        <CloseFileButton isDirty={isDirty} onClick={e => onClose(file.id, e)} />
+      </TabItemComponent>
+    )
+  }
 )
